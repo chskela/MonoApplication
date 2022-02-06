7a29f7764ceca2138700f5ef197b3286583f3aae
Chistaykov Aleksey <chskela@gmail.com>
2022-02-06 21:35:22 +0300

    Update color palettes

--- app/src/main/java/com/chskela/monoapplication/ui/theme/Color.kt
+++ app/src/main/java/com/chskela/monoapplication/ui/theme/Color.kt
@@ -2,7 +2,21 @@ package com.chskela.monoapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Red = Color(0xFFFE7474)
+val Blue = Color(0xFF5986F2)
+val Branding = Color(0xFF5986F2)
+val Expense = Color(0xFFFF9A9A)
+val Income = Color(0xFF9AB8FF)
+
+val PrimaryDark = Color(0xFFEAEAEA)
+val SecondaryDark = Color(0xFFBABABF)
+val GrayDark = Color(0xFF898A93)
+val BackgroundDark = Color(0xFF595968)
+val BlackDark = Color(0xFF28293D)
+
+val Primary = Color(0xFF404040)
+val Secondary = Color(0xFFA6A6A6)
+val Gray = Color(0xFFD9D9D9)
+val Background = Color(0xFFF5F5F5)
+val White = Color(0xFFFCFCFC)
+
