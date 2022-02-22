028eabb60509880b0c42dcd1f1d5885de3a627e1
Chistaykov Aleksey <chskela@gmail.com>
2022-02-22 19:24:12 +0300

    Added MonoTextField.kt

---
+++ app/src/main/java/com/chskela/monoapplication/presentation/ui/components/textfield/MonoTextField.kt
@@ -0,0 +1,45 @@
+package com.chskela.monoapplication.presentation.ui.components.textfield
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MonoTextField(
+    label: String = "",
+    value: String,
+    textStyle: TextStyle = MaterialTheme.typography.subtitle2,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    singleLine: Boolean = true,
+    maxLines: Int = 5,
+    onValueChange: (String) -> Unit = {},
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+) {
+    Text(text = label,
+        style = MaterialTheme.typography.h6,
+        color = MaterialTheme.colors.secondary)
+    Spacer(modifier = Modifier.height(8.dp))
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        trailingIcon = trailingIcon,
+        leadingIcon = leadingIcon,
+        textStyle = textStyle,
+        shape = MaterialTheme.shapes.medium,
+        placeholder = placeholder,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        value = value,
+        onValueChange = { onValueChange(it) },
+        keyboardOptions = keyboardOptions
+    )
+}
