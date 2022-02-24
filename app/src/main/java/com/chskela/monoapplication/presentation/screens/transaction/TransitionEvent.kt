167ef8f9e82fa2453037d7366755ca7a1548d37e
Chistaykov Aleksey <chskela@gmail.com>
2022-02-24 18:37:17 +0300

    Added TransitionEvent.kt

---
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/transaction/TransitionEvent.kt
@@ -0,0 +1,11 @@
+package com.chskela.monoapplication.presentation.screens.transaction
+
+sealed class TransitionEvent{
+    data class SelectTab(val tab: Int) : TransitionEvent()
+    data class ChangeAmount(val value: String) : TransitionEvent()
+    data class ChangeNote(val value: String) : TransitionEvent()
+    data class SelectCategory(val categoryId: Long) : TransitionEvent()
+    object Submit : TransitionEvent()
+    object PreviousData: TransitionEvent()
+    object NextData: TransitionEvent()
+}
