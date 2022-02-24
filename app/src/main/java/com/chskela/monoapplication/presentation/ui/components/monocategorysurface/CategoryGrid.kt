263ff0653eb34478a97f703a57ab06a386b3d704
Chistaykov Aleksey <chskela@gmail.com>
2022-02-24 14:41:16 +0300

    Fix CategoryGrid.kt - padding

--- app/src/main/java/com/chskela/monoapplication/presentation/ui/components/monocategorysurface/CategoryGrid.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/ui/components/monocategorysurface/CategoryGrid.kt
@@ -22,21 +22,21 @@ fun CategoryGrid(
     ) { measurables, constraints ->
         val placeables = measurables.map { it.measure(constraints) }
         val ratio = constraints.maxWidth / placeables[0].width
-        val padding = (constraints.maxWidth - placeables[0].width * ratio) / ratio
+        val padding = (constraints.maxWidth - placeables[0].width * ratio) / (ratio - 1)
         layout(
             width = placeables[0].width * ratio + padding * (ratio - 1),
             height = (placeables[0].height + padding) * ceil(placeables.size.toDouble() / ratio).toInt()
         ) {
             var xPosition = 0
             var yPosition = 0
-            placeables.forEach { placeable ->
+            placeables.forEachIndexed { index, placeable ->
                 placeable.place(
                     x = xPosition,
                     y = yPosition
                 )
-                val isNextRow = xPosition + placeable.width * 2 > constraints.maxWidth
-                xPosition = if (!isNextRow) xPosition + placeable.width + padding else 0
-                yPosition = if (isNextRow) yPosition + placeable.height + padding else yPosition
+                val isLastInRow = (index + 1) % ratio == 0
+                xPosition = if (!isLastInRow) xPosition + placeable.width + padding else 0
+                yPosition = if (isLastInRow) yPosition + placeable.height + padding else yPosition
             }
         }
     }
