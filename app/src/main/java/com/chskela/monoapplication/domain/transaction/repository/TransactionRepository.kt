36a961dc23c58a475399d87b0383668cb5493e51
Chistaykov Aleksey <chskela@gmail.com>
2022-02-26 23:53:00 +0300

    Fix TransactionRepository.kt - fix type param Transaction

--- app/src/main/java/com/chskela/monoapplication/domain/transaction/repository/TransactionRepository.kt
+++ app/src/main/java/com/chskela/monoapplication/domain/transaction/repository/TransactionRepository.kt
@@ -1,17 +1,17 @@
 package com.chskela.monoapplication.domain.transaction.repository
 
-import com.chskela.monoapplication.data.transaction.storage.models.TransactionEntity
+import com.chskela.monoapplication.domain.transaction.models.Transaction
 import kotlinx.coroutines.flow.Flow
 
 interface TransactionRepository {
 
-    fun getAllTransaction(): Flow<List<TransactionEntity>>
+    fun getAllTransactions(): Flow<List<Transaction>>
 
-    suspend fun getTransactionById(id: Long): TransactionEntity
+    suspend fun getTransactionById(id: Long): Transaction
 
-    suspend fun insertTransaction(transactionEntity: TransactionEntity)
+    suspend fun insertTransaction(transaction: Transaction)
 
-    suspend fun updateTransaction(transactionEntity: TransactionEntity)
+    suspend fun updateTransaction(transaction: Transaction)
 
-    suspend fun deleteTransaction(transactionEntity: TransactionEntity)
+    suspend fun deleteTransaction(transaction: Transaction)
 }
