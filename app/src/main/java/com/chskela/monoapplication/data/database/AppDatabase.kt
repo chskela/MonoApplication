54c3c268c0217f447cf1e065b1da93db8402482a
Chistaykov Aleksey <chskela@gmail.com>
2022-02-26 23:51:41 +0300

    Update AppDatabase.kt - added TransactionDao

--- app/src/main/java/com/chskela/monoapplication/data/database/AppDatabase.kt
+++ app/src/main/java/com/chskela/monoapplication/data/database/AppDatabase.kt
@@ -6,11 +6,14 @@ import com.chskela.monoapplication.data.category.storage.dao.CategoryDao
 import com.chskela.monoapplication.data.category.storage.models.CategoryEntity
 import com.chskela.monoapplication.data.currency.storage.dao.CurrencyDao
 import com.chskela.monoapplication.data.currency.storage.models.CurrencyEntity
+import com.chskela.monoapplication.data.transaction.storage.dao.TransactionDao
+import com.chskela.monoapplication.data.transaction.storage.models.TransactionEntity
 
-@Database(entities = [CurrencyEntity::class, CategoryEntity::class], version = 3)
+@Database(entities = [CurrencyEntity::class, CategoryEntity::class, TransactionEntity::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract val currencyDao: CurrencyDao
     abstract val categoryDao: CategoryDao
+    abstract val transactionDao: TransactionDao
 
     companion object {
         const val DATABASE_NAME = "mono_db"
