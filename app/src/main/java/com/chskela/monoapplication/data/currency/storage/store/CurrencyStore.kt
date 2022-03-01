885ab51a8f5e8c3d3524a4c0a54becbdf02e6f57
Chistaykov Aleksey <chskela@gmail.com>
2022-03-01 22:45:16 +0300

    Added CurrencyStore.kt - DataStore for currency

---
+++ app/src/main/java/com/chskela/monoapplication/data/currency/storage/store/CurrencyStore.kt
@@ -0,0 +1,39 @@
+package com.chskela.monoapplication.data.currency.storage.store
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class CurrencyStore(private val context: Context) {
+
+    companion object {
+        private const val CURRENCY_DEFAULT = "CURRENCY_DEFAULT"
+        private val CURRENCY_DEFAULT_KEY = longPreferencesKey(CURRENCY_DEFAULT)
+        private val Context.dataStore by preferencesDataStore("setting")
+    }
+
+    val getDefaultCurrency: Flow<Long> = context.dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+        .map { preferences ->
+            preferences[CURRENCY_DEFAULT_KEY] ?: 1
+        }
+
+    suspend fun saveDefaultCurrency(id: Long) {
+        context.dataStore.edit { preferences ->
+            preferences[CURRENCY_DEFAULT_KEY] = id
+        }
+    }
+
+}
