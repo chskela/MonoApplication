889448f2dd18b299e1fe84b1e09ec641d61e71ba
Chistaykov Aleksey <chskela@gmail.com>
2022-03-01 22:51:18 +0300

    Update DataModule.kt - add provideCurrencyStore and provideTransactionRepository

--- app/src/main/java/com/chskela/monoapplication/di/DataModule.kt
+++ app/src/main/java/com/chskela/monoapplication/di/DataModule.kt
@@ -1,15 +1,20 @@
 package com.chskela.monoapplication.di
 
 import android.app.Application
+import android.content.Context
 import androidx.room.Room
 import com.chskela.monoapplication.data.category.repository.CategoryRepositoryImpl
 import com.chskela.monoapplication.data.currency.repository.CurrencyRepositoryImpl
+import com.chskela.monoapplication.data.currency.storage.store.CurrencyStore
 import com.chskela.monoapplication.data.database.AppDatabase
+import com.chskela.monoapplication.data.transaction.repository.TransactionRepositoryImpl
 import com.chskela.monoapplication.domain.category.repository.CategoryRepository
 import com.chskela.monoapplication.domain.currency.repository.CurrencyRepository
+import com.chskela.monoapplication.domain.transaction.repository.TransactionRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -32,14 +37,25 @@ object DataModule {
 
     @Provides
     @Singleton
-    fun provideCurrencyRepository(db: AppDatabase): CurrencyRepository {
-        return CurrencyRepositoryImpl(db.currencyDao)
+    fun provideCurrencyStore(@ApplicationContext appContext: Context): CurrencyStore {
+        return CurrencyStore(appContext)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCurrencyRepository(db: AppDatabase, currencyStore: CurrencyStore): CurrencyRepository {
+        return CurrencyRepositoryImpl(db.currencyDao, currencyStore)
     }
 
     @Provides
     @Singleton
     fun provideCategoryRepository(db: AppDatabase): CategoryRepository {
         return CategoryRepositoryImpl(db.categoryDao)
+    }
+    @Provides
+    @Singleton
+    fun provideTransactionRepository(db: AppDatabase): TransactionRepository {
+        return TransactionRepositoryImpl(db.transactionDao)
     }
 
 }
