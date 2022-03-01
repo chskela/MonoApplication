a516edc233abb8a79db5017185a39cefabe42003
Chistaykov Aleksey <chskela@gmail.com>
2022-03-01 22:51:48 +0300

    Update Transaction.kt - int to long

--- app/src/main/java/com/chskela/monoapplication/domain/transaction/models/Transaction.kt
+++ app/src/main/java/com/chskela/monoapplication/domain/transaction/models/Transaction.kt
@@ -4,7 +4,7 @@ package com.chskela.monoapplication.domain.transaction.models
 data class Transaction(
     val id: Long,
     val timestamp: Long,
-    val amount: Int,
+    val amount: Long,
     val note: String,
     val categoryId: Long,
     val currencyId: Long,
