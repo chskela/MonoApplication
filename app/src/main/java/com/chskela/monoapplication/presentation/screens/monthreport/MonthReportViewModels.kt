75ffbe28e68cfd87416ffdb263cf72f6bf1f51f9
Chistaykov Aleksey <chskela@gmail.com>
2022-03-10 20:30:36 +0300

    Update MonthReportViewModels - added formatDate and calendar

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/monthreport/MonthReportViewModels.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/monthreport/MonthReportViewModels.kt
@@ -5,11 +5,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.chskela.monoapplication.presentation.screens.monthreport.models.MonthReportUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
-class MonthReportViewModels @Inject constructor() : ViewModel() {
-    var uiState: MutableState<MonthReportUiState> = mutableStateOf(MonthReportUiState())
+class MonthReportViewModels @Inject constructor(
+//    private val getAllTransactionsUseCase: GetAllTransactionsUseCase
+) : ViewModel() {
+
+    private var calendar = Calendar.getInstance()
+
+    var uiState: MutableState<MonthReportUiState> = mutableStateOf(MonthReportUiState(
+        currentData = formatDate(calendar.time)
+    ))
         private set
 
+
+    private fun formatDate(date: Date) =
+        SimpleDateFormat("MMMM, yyyy", Locale.getDefault()).format(date)
 }
