99864de909b22f5dd88ebc1f736fa0a155ae75c2
Chistaykov Aleksey <chskela@gmail.com>
2022-03-10 20:29:17 +0300

    Update MonthReportUiState - Long to Double

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/monthreport/models/MonthReportUiState.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/monthreport/models/MonthReportUiState.kt
@@ -1,14 +1,13 @@
 package com.chskela.monoapplication.presentation.screens.monthreport.models
 
-import com.chskela.monoapplication.domain.transaction.models.Transaction
-
 data class MonthReportUiState(
-    val currentData: String = "Feb 24, 2022 (Sat)",
-    val currentBalance: Long = 0,
-    val income: Long = 0,
-    val expense: Long = 0,
-    val expenseIncome: Long = 0,
-    val previousBalance: Long = 0,
+    val currentData: String = "February, 2022",
+    val currency: String = "",
+    val currentBalance: Double = 0.0,
+    val income: Double = 0.0,
+    val expense: Double = 0.0,
+    val expenseIncome: Double = 0.0,
+    val previousBalance: Double = 0.0,
     val currentTab: Int = 0,
-    val transactionList: List<Transaction> = emptyList()
+    val transactionList: List<TransactionUi> = emptyList()
 )
