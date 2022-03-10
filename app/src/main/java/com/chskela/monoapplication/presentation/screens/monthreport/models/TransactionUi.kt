d8567f8b186dbb120d8a92aa758be15dda15b57a
Chistaykov Aleksey <chskela@gmail.com>
2022-03-10 20:25:15 +0300

    Added TransactionUi.kt

---
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/monthreport/models/TransactionUi.kt
@@ -0,0 +1,11 @@
+package com.chskela.monoapplication.presentation.screens.monthreport.models
+
+data class TransactionUi(
+    val id: Long,
+    val timestamp: Long,
+    val amount: Long,
+    val note: String,
+    val type: TypeTransaction = TypeTransaction.Expense,
+    val category: String,
+    val icon: Int
+)
