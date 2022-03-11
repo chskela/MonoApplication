70c7f57b53b811d52f2246c14580e9fac2a548e6
Chistaykov Aleksey <chskela@gmail.com>
2022-03-11 23:22:37 +0300

    Update OnBoardingScreen - added onEvent

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/onboarding/OnBoardingScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chskela.monoapplication.R
 import com.chskela.monoapplication.presentation.screens.onboarding.models.OnBoardingPage
 import com.chskela.monoapplication.presentation.screens.onboarding.models.OnBoardingUiState
@@ -27,51 +26,55 @@ import com.chskela.monoapplication.presentation.ui.components.button.MonoButton
 import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme
 
 @Composable
-fun OnBoardingActivityScreen(onBoardingViewModel: OnBoardingViewModel = hiltViewModel()) {
-        val uiState = onBoardingViewModel.uiState.value
-        val page = uiState.onBoardingPage.page
-        OnBoardingScreen(
-            uiState = uiState,
-            onClickButton = {
-                onBoardingViewModel.onEvent(
-                    OnBoardingEvent.NextPage(page)
-                )
-            }
-        )
+fun OnBoardingActivityScreen(
+    onBoardingViewModel: OnBoardingViewModel = hiltViewModel(),
+) {
+    val uiState = onBoardingViewModel.uiState.value
+    OnBoardingScreen(
+        uiState = uiState,
+        onEvent = onBoardingViewModel::onEvent
+    )
 }
 
 @Composable
 fun OnBoardingScreen(
     uiState: OnBoardingUiState,
-    onSkip: () -> Unit = {},
-    onClickButton: () -> Unit = {},
+    onEvent: (OnBoardingEvent) -> Unit = {},
 ) {
     val (page, image, title, body, buttonTitle) = uiState.onBoardingPage
 
     Surface {
-        Log.e("RESULT", "Screen")
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp, vertical = 24.dp),
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp, vertical = 24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween) {
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 40.dp),
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 40.dp),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text(text = "${page.pageNumber}/3",
+                    Text(
+                        text = "${page.pageNumber}/3",
                         style = MaterialTheme.typography.body1,
-                        color = MaterialTheme.colors.onSurface)
+                        color = MaterialTheme.colors.onSurface
+                    )
 
                     if (uiState.skip) {
-                        OutlinedButton(onClick = onSkip,
-                            contentPadding = PaddingValues(horizontal = 24.dp)) {
-                            Text(text = "Skip",
+                        OutlinedButton(
+                            onClick = { onEvent(OnBoardingEvent.Skip) },
+                            contentPadding = PaddingValues(horizontal = 24.dp)
+                        ) {
+                            Text(
+                                text = "Skip",
                                 style = MaterialTheme.typography.body1,
-                                color = MaterialTheme.colors.onSurface)
+                                color = MaterialTheme.colors.onSurface
+                            )
                         }
                     }
                 }
@@ -89,10 +92,14 @@ fun OnBoardingScreen(
                     style = MaterialTheme.typography.h2,
                     color = MaterialTheme.colors.onSurface
                 )
-                Text(text = stringResource(id = body),
-                    style = MaterialTheme.typography.body1)
+                Text(
+                    text = stringResource(id = body),
+                    style = MaterialTheme.typography.body1
+                )
             }
-            MonoButton(text = stringResource(id = buttonTitle), onClick = onClickButton)
+            MonoButton(
+                text = stringResource(id = buttonTitle),
+                onClick = { onEvent(OnBoardingEvent.NextPage(page)) })
         }
     }
 }
@@ -102,15 +109,17 @@ fun OnBoardingScreen(
 @Composable
 fun PreviewOnBoardingScreen() {
     MonoApplicationTheme {
-        OnBoardingScreen(uiState = OnBoardingUiState(
-            onBoardingPage = OnBoardingPage(
-                page = Pages.First,
-                image = R.drawable.on_boarding_step1,
-                title = R.string.on_boarding_title_step1,
-                body = R.string.on_boarding_body_step1,
-                buttonTitle = R.string.on_boarding_continue,
-            ),
-            skip = true
-        ))
+        OnBoardingScreen(
+            uiState = OnBoardingUiState(
+                onBoardingPage = OnBoardingPage(
+                    page = Pages.First,
+                    image = R.drawable.on_boarding_step1,
+                    title = R.string.on_boarding_title_step1,
+                    body = R.string.on_boarding_body_step1,
+                    buttonTitle = R.string.on_boarding_continue,
+                ),
+                skip = true
+            )
+        )
     }
 }
