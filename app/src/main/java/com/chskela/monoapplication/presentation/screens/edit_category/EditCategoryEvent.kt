088227fbeabe0f178268ec01f4fa2ccc32a673a1
Chistaykov Aleksey <chskela@gmail.com>
2022-03-25 20:28:58 +0300

    Update EditCategoryEvent - added ChangeCategoryName and ChangeCategoryIcon

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/edit_category/EditCategoryEvent.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/edit_category/EditCategoryEvent.kt
@@ -5,5 +5,7 @@ import com.chskela.monoapplication.presentation.ui.components.categorysurface.Ca
 sealed class EditCategoryEvent{
     data class GetCategory(val categoryId: Long) : EditCategoryEvent()
     data class EditCategory(val categoryUi: CategoryUi) : EditCategoryEvent()
+    data class ChangeCategoryName(val value: String): EditCategoryEvent()
+    data class ChangeCategoryIcon(val iconId: Long): EditCategoryEvent()
     object AddCategory : EditCategoryEvent()
 }
