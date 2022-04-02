c3cb3c216a02a763519ed94e5be7cedb1c405069
Chistaykov Aleksey <chskela@gmail.com>
2022-04-02 17:38:57 +0300

    Update CategoryEvent - DeleteCategory

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/category/CategoryEvent.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/category/CategoryEvent.kt
@@ -1,8 +1,5 @@
 package com.chskela.monoapplication.presentation.screens.category
 
-import com.chskela.monoapplication.presentation.ui.components.categorysurface.CategoryUi
-
 sealed class CategoryEvent{
-    data class EditCategory(val categoryUi: CategoryUi) : CategoryEvent()
-    object AddMore : CategoryEvent()
+    data class DeleteCategory(val id: Long) : CategoryEvent()
 }
