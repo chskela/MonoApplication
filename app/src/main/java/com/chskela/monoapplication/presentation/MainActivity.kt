98585a7aa9c83be1e6f5d16c29a65a0160f5ef6f
Chistaykov Aleksey <chskela@gmail.com>
2022-04-02 12:29:47 +0300

    Update MainActivity - added lateinit var mainActivityViewModel, installSplashScreen

--- app/src/main/java/com/chskela/monoapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/MainActivity.kt
@@ -3,15 +3,25 @@ package com.chskela.monoapplication.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @Inject
+    lateinit var mainActivityViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().setKeepOnScreenCondition{
+            !mainActivityViewModel.isLoading.value
+        }
+
         setContent {
-            MonoApp()
+            MonoApp(onBoardingIsSkip = mainActivityViewModel.onBoardingIsSkip.value)
         }
     }
 }
