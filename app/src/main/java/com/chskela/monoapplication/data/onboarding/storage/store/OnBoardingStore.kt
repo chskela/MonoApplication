7a4f634ecaaaaec9c66bb1eecd94fc080bd0faea
Chistaykov Aleksey <chskela@gmail.com>
2022-04-02 12:21:48 +0300

    Added OnBoardingStore

---
+++ app/src/main/java/com/chskela/monoapplication/data/onboarding/storage/store/OnBoardingStore.kt
@@ -0,0 +1,39 @@
+package com.chskela.monoapplication.data.onboarding.storage.store
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class OnBoardingStore(private val context: Context) {
+
+    companion object {
+        private const val ON_BOARDING_SKIP_DEFAULT = "ON_BOARDING_SKIP_DEFAULT"
+        private val ON_BOARDING_DEFAULT_KEY = booleanPreferencesKey(ON_BOARDING_SKIP_DEFAULT)
+        private val Context.dataStore by preferencesDataStore("setting")
+    }
+
+    val onBoardingIsSkip: Flow<Boolean> = context.dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+        .map { preferences ->
+            preferences[ON_BOARDING_DEFAULT_KEY] ?: true
+        }
+
+    suspend fun setOnBoardingIsSkip() {
+        context.dataStore.edit { preferences ->
+            preferences[ON_BOARDING_DEFAULT_KEY] = false
+        }
+    }
+
+}
