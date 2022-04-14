3a018906978039448730f4a2c913b2cb8b77b236
Chistaykov Aleksey <chskela@gmail.com>
2022-04-14 12:36:10 +0300

    Added reportGraph

--- app/src/main/java/com/chskela/monoapplication/presentation/navigation/MonoScreens.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/navigation/MonoScreens.kt
@@ -1,14 +1,21 @@
-package com.chskela.monoapplication.navigation
+package com.chskela.monoapplication.presentation.navigation
 
 enum class MonoScreens {
     OnBoarding,
     Transaction,
+
+    ReportRoot,
     MonthReport,
+    CategoryReport,
+    CategoryReportDetails,
+
     SettingRoot,
     Setting,
+
     CategoryRoot,
     Category,
     EditCategory,
     AddCategory,
+
     Currency
 }
---
+++ app/src/main/java/com/chskela/monoapplication/presentation/navigation/reportGraph.kt
@@ -0,0 +1,41 @@
+package com.chskela.monoapplication.presentation.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import com.chskela.monoapplication.presentation.screens.categoryreport.CategoryReportActivityScreen
+import com.chskela.monoapplication.presentation.screens.monthreport.MonthReportActivityScreen
+
+fun NavGraphBuilder.reportGraph(navController: NavController) {
+    navigation(
+        startDestination = MonoScreens.MonthReport.name,
+        route = MonoScreens.ReportRoot.name
+    ) {
+
+        composable(MonoScreens.MonthReport.name) {
+            MonthReportActivityScreen()
+        }
+
+        composable(MonoScreens.CategoryReport.name) {
+            fun onClick(id: Long) {
+                navController.currentBackStackEntry?.arguments?.putLong(
+                    "categoryId",
+                    id
+                )
+                navController.navigate(MonoScreens.CategoryReportDetails.name)
+            }
+            CategoryReportActivityScreen(
+                onSelectCategory = ::onClick
+            )
+        }
+
+        composable(MonoScreens.CategoryReportDetails.name) {
+            val categoryId =
+                navController.previousBackStackEntry?.arguments?.getLong("categoryId")
+            categoryId?.let {
+                // TODO
+            }
+        }
+    }
+}
