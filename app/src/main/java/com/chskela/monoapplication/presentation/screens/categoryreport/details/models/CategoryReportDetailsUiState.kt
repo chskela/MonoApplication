6f96f753de745eee57fb5198154049db2f859e65
Chistaykov Aleksey <chskela@gmail.com>
2022-04-14 18:01:50 +0300

    Update CategoryReportDetailsUiState - added  categoryName, icon, typeCategory

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/details/models/CategoryReportDetailsUiState.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/details/models/CategoryReportDetailsUiState.kt
@@ -1,6 +1,11 @@
 package com.chskela.monoapplication.presentation.screens.categoryreport.details.models
 
+import com.chskela.monoapplication.domain.category.models.TypeCategory
+
 data class CategoryReportDetailsUiState(
     val currentTab: Int = 0,
     val currentCategory: Long = 0,
+    val categoryName: String = "",
+    val icon: String = "",
+    val typeCategory: TypeCategory = TypeCategory.Expense,
 )
