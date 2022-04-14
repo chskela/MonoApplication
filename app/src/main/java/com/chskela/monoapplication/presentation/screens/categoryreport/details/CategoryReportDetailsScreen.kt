299b4d6fc80fe3ae4055ff584d80d74d0ecf8eec
Chistaykov Aleksey <chskela@gmail.com>
2022-04-14 17:24:33 +0300

    Refactoring CategoryReportDetailsScreen - extract DetailsTabs

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/details/CategoryReportDetailsScreen.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/details/CategoryReportDetailsScreen.kt
@@ -1,22 +1,16 @@
 package com.chskela.monoapplication.presentation.screens.categoryreport.details
 
 import android.content.res.Configuration
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -24,7 +18,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.chskela.monoapplication.R
-import com.chskela.monoapplication.presentation.screens.categoryreport.models.CategoryReportUiState
+import com.chskela.monoapplication.presentation.screens.categoryreport.details.components.DetailsTabs
+import com.chskela.monoapplication.presentation.screens.categoryreport.details.models.CategoryReportDetailsUiState
 import com.chskela.monoapplication.presentation.ui.components.topappbar.MonoTopAppBar
 import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme
 
@@ -35,13 +30,15 @@ fun CategoryReportDetailsActivityScreen(
 ) {
     CategoryReportDetailsScreen(
         uiState = categoryReportDetailsViewModels.uiState.value,
+        onEvent = categoryReportDetailsViewModels::onEvent,
         onBack = onBack
     )
 }
 
 @Composable
 fun CategoryReportDetailsScreen(
-    uiState: CategoryReportUiState,
+    uiState: CategoryReportDetailsUiState,
+    onEvent: (CategoryReportDetailsEvent) -> Unit = {},
     onBack: () -> Unit = {}
 ) {
     val scrollState = rememberScrollState()
@@ -75,70 +72,11 @@ fun CategoryReportDetailsScreen(
         ) {
             Spacer(modifier = Modifier.height(24.dp))
 
-            TabRow(
-                modifier = Modifier
-                    .size(height = 48.dp, width = 199.dp)
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colors.secondaryVariant,
-                        shape = MaterialTheme.shapes.small
-                    )
-                    .background(
-                        color = MaterialTheme.colors.surface,
-                        shape = MaterialTheme.shapes.small
-                    )
-                    .padding(4.dp),
-                selectedTabIndex = 1,
-                indicator = @Composable { },
-                divider = {},
-                backgroundColor = MaterialTheme.colors.surface
-            ) {
-                listOf("Month", "Week").forEachIndexed { index, title ->
-                    val selected = 1 == index
-                    val color: Color by animateColorAsState(
-                        targetValue = if (selected) {
-                            MaterialTheme.colors.onSurface
-                        } else {
-                            MaterialTheme.colors.secondary
-                        },
-                        animationSpec = spring(
-                            dampingRatio = Spring.DampingRatioHighBouncy,
-                            stiffness = Spring.StiffnessMedium
-                        )
-                    )
-                    val background: Color by animateColorAsState(
-                        targetValue = if (selected) {
-                            MaterialTheme.colors.background
-                        } else {
-                            MaterialTheme.colors.surface
-                        },
-                        animationSpec = spring(
-                            dampingRatio = Spring.DampingRatioHighBouncy,
-                            stiffness = Spring.StiffnessMedium
-                        )
-                    )
-                    val shape = if (index == 0) {
-                        RoundedCornerShape(topStart = 24.dp, bottomStart = 24.dp)
-                    } else RoundedCornerShape(topEnd = 24.dp, bottomEnd = 24.dp)
-                    Tab(
-                        modifier = Modifier
-                            .background(
-                                color = background,
-                                shape = shape
-                            )
-                            .clip(shape = shape),
-                        text = {
-                            Text(
-                                text = title,
-                                style = MaterialTheme.typography.body1,
-                                color = color
-                            )
-                        },
-                        selected = selected,
-                        onClick = { }
-                    )
-                }
-            }
+            DetailsTabs(
+                state = uiState.currentTab,
+                onSelect = { onEvent(CategoryReportDetailsEvent.SelectTab(it)) }
+            )
+
         }
     }
 }
@@ -149,7 +87,7 @@ fun CategoryReportDetailsScreen(
 fun PreviewCategoryReportDetailsScreen() {
     MonoApplicationTheme {
         CategoryReportDetailsScreen(
-            uiState = CategoryReportUiState()
+            uiState = CategoryReportDetailsUiState()
         )
 
 
---
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/details/components/DetailsTabs.kt
@@ -0,0 +1,93 @@
+package com.chskela.monoapplication.presentation.screens.categoryreport.details.components
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.chskela.monoapplication.R
+
+@Composable
+fun DetailsTabs(
+    state: Int,
+    onSelect: (Int) -> Unit = {}
+) {
+    TabRow(
+        modifier = Modifier
+            .size(height = 48.dp, width = 199.dp)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colors.secondaryVariant,
+                shape = MaterialTheme.shapes.small
+            )
+            .background(
+                color = MaterialTheme.colors.surface,
+                shape = MaterialTheme.shapes.small
+            )
+            .padding(4.dp),
+        selectedTabIndex = 1,
+        indicator = @Composable { },
+        divider = {},
+        backgroundColor = MaterialTheme.colors.surface
+    ) {
+        listOf(
+            stringResource(id = R.string.month),
+            stringResource(id = R.string.week)
+        ).forEachIndexed { index, title ->
+            val selected = state == index
+            val color: Color by animateColorAsState(
+                targetValue = if (selected) {
+                    MaterialTheme.colors.onSurface
+                } else {
+                    MaterialTheme.colors.secondary
+                }
+            )
+            val background: Color by animateColorAsState(
+                targetValue = if (selected) {
+                    MaterialTheme.colors.background
+                } else {
+                    MaterialTheme.colors.surface
+                },
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioHighBouncy,
+                    stiffness = Spring.StiffnessMedium
+                )
+            )
+            val shape = if (index == 0) {
+                RoundedCornerShape(topStart = 24.dp, bottomStart = 24.dp)
+            } else RoundedCornerShape(topEnd = 24.dp, bottomEnd = 24.dp)
+
+            Tab(
+                modifier = Modifier
+                    .background(
+                        color = background,
+                        shape = shape
+                    )
+                    .clip(shape = shape),
+                text = {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.body1,
+                        color = color
+                    )
+                },
+                selected = selected,
+                onClick = { onSelect(index) }
+            )
+        }
+    }
+}
