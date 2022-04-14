f0a14e788fdbb1fa7539cd6e4a0b7fb86898ea0e
Chistaykov Aleksey <chskela@gmail.com>
2022-04-14 17:25:09 +0300

    Added CategoryReportDetailsUiState

---
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/details/models/CategoryReportDetailsUiState.kt
@@ -0,0 +1,6 @@
+package com.chskela.monoapplication.presentation.screens.categoryreport.details.models
+
+data class CategoryReportDetailsUiState(
+    val currentTab: Int = 0,
+    val currentCategory: Long = 0,
+)
