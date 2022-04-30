1cd198b70ad9e0e5af1a51f2bf0d62054e9ddd91
Chistaykov Aleksey <chskela@gmail.com>
2022-04-30 19:02:27 +0300

    Update CategoryReportDetailsScreen - add ReportChart and MonoTransactionList

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/details/CategoryReportDetailsScreen.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/details/CategoryReportDetailsScreen.kt
@@ -2,8 +2,6 @@ package com.chskela.monoapplication.presentation.screens.categoryreport.details
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,18 +10,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.chskela.monoapplication.R
-import com.chskela.monoapplication.data.icons.iconsMap
 import com.chskela.monoapplication.domain.category.models.TypeCategory
 import com.chskela.monoapplication.presentation.screens.categoryreport.details.components.DetailsBigIcon
 import com.chskela.monoapplication.presentation.screens.categoryreport.details.components.DetailsTabs
+import com.chskela.monoapplication.presentation.screens.categoryreport.details.components.ReportChart
 import com.chskela.monoapplication.presentation.screens.categoryreport.details.models.CategoryReportDetailsUiState
-import com.chskela.monoapplication.presentation.screens.monthreport.models.TypeTransaction
 import com.chskela.monoapplication.presentation.ui.components.topappbar.MonoTopAppBar
+import com.chskela.monoapplication.presentation.ui.components.transactionList.MonoTransactionList
 import com.chskela.monoapplication.presentation.ui.theme.Expense
 import com.chskela.monoapplication.presentation.ui.theme.Income
 import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme
@@ -113,67 +110,9 @@ fun CategoryReportDetailsScreen(
                 style = MaterialTheme.typography.h1,
                 color = color
             )
+            ReportChart(graphColor = color)
 
-            Row {
-                LazyColumn(modifier = Modifier.padding(vertical = 16.dp)) {
-                    items(items = uiState.transactionList) { transactionUi ->
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(bottom = 16.dp, start = 16.dp, end = 16.dp),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Column(modifier = Modifier.weight(0.7f)) {
-                                Row(verticalAlignment = Alignment.CenterVertically) {
-                                    val icon = iconsMap[transactionUi.icon]
-                                    icon?.let { id ->
-                                        Icon(
-                                            imageVector = ImageVector.vectorResource(id = id),
-                                            contentDescription = transactionUi.category
-                                        )
-                                    }
-
-                                    Spacer(modifier = Modifier.size(12.dp))
-                                    Text(
-                                        text = transactionUi.category,
-                                        style = MaterialTheme.typography.body1,
-                                        color = MaterialTheme.colors.onSurface,
-                                    )
-                                    if (transactionUi.note.isNotBlank()) {
-                                        Text(
-                                            text = " (${transactionUi.note})",
-                                            style = MaterialTheme.typography.caption,
-                                            color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f),
-                                            overflow = TextOverflow.Ellipsis,
-                                            maxLines = 1
-                                        )
-                                    }
-                                }
-                            }
-                            Column(
-                                modifier = Modifier.weight(0.3f),
-                                verticalArrangement = Arrangement.Center,
-                                horizontalAlignment = Alignment.End
-                            ) {
-                                val prefix = when (transactionUi.type) {
-                                    TypeTransaction.Expense -> "-"
-                                    TypeTransaction.Income -> "+"
-                                }
-                                val color = when (transactionUi.type) {
-                                    TypeTransaction.Expense -> Expense
-                                    TypeTransaction.Income -> Income
-                                }
-
-                                Text(
-                                    text = "$prefix${uiState.currency}${transactionUi.amount}",
-                                    style = MaterialTheme.typography.body1,
-                                    color = color
-                                )
-                            }
-                        }
-                    }
-                }
-            }
+            MonoTransactionList(transactionList =  uiState.transactionList, currency = uiState.currency)
         }
     }
 }
@@ -186,7 +125,5 @@ fun PreviewCategoryReportDetailsScreen() {
         CategoryReportDetailsScreen(
             uiState = CategoryReportDetailsUiState()
         )
-
-
     }
 }
