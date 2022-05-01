017252298404096adb357bda7036998a514ff2cd
Chistaykov Aleksey <chskela@gmail.com>
2022-05-01 18:27:38 +0300

    Update MonthReportScreen - remove uiState

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/monthreport/MonthReportScreen.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/monthreport/MonthReportScreen.kt
@@ -2,129 +2,103 @@ package com.chskela.monoapplication.presentation.screens.monthreport
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.chskela.monoapplication.R
 import com.chskela.monoapplication.presentation.screens.monthreport.components.BorderRow
-import com.chskela.monoapplication.presentation.screens.monthreport.models.MonthReportUiState
 import com.chskela.monoapplication.presentation.screens.monthreport.models.TransactionUi
 import com.chskela.monoapplication.presentation.screens.monthreport.models.TypeTransaction
+import com.chskela.monoapplication.presentation.screens.reports.ReportsEvent
 import com.chskela.monoapplication.presentation.ui.components.datarange.MonoDateRange
 import com.chskela.monoapplication.presentation.ui.components.tabs.MonoTabs
-import com.chskela.monoapplication.presentation.ui.components.topappbar.MonoTopAppBar
 import com.chskela.monoapplication.presentation.ui.components.transactionList.MonoTransactionList
 import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme
 import kotlin.math.absoluteValue
 
-
-@Composable
-fun MonthReportActivityScreen(
-    monthReportViewModels: MonthReportViewModels = hiltViewModel(),
-) {
-    MonthReportScreen(
-        uiState = monthReportViewModels.uiState.value,
-        onEvent = monthReportViewModels::onEvent,
-    )
-}
-
 @Composable
 fun MonthReportScreen(
-    uiState: MonthReportUiState,
-    onEvent: (MonthReportEvent) -> Unit = {},
+    currentData: String = "February, 2022",
+    currency: String = "",
+    currentBalance: Double = 0.0,
+    income: Double = 0.0,
+    expense: Double = 0.0,
+    expenseIncome: Double = 0.0,
+    previousBalance: Double = 0.0,
+    currentTab: Int = 1,
+    transactionList: List<TransactionUi> = emptyList(),
+    onEvent: (ReportsEvent) -> Unit = {},
 ) {
-    Scaffold(
-        topBar = {
-            MonoTopAppBar(
-                title = stringResource(id = R.string.month_report),
-                navigationIcon = {
-                    Spacer(modifier = Modifier.size(48.dp))
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = ImageVector.vectorResource(id = R.drawable.caret_down),
-                            tint = MaterialTheme.colors.secondary,
-                            contentDescription = "caret down"
-                        )
+    val incomeStr = stringResource(id = R.string.income)
+    val expenseStr = stringResource(id = R.string.expense)
+    fun sign(value: Double) = if (value >= 0) "+" else "-"
+
+    Column {
+        Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+            Column {
+
+                Spacer(modifier = Modifier.size(24.dp))
+
+                MonoDateRange(
+                    currentDate = currentData,
+                    onPrevious = { onEvent(ReportsEvent.PreviousMonth) },
+                    onNext = { onEvent(ReportsEvent.NextMonth) })
+
+                Spacer(modifier = Modifier.size(24.dp))
+
+                BorderRow(content = listOf {
+                    Text(text = stringResource(id = R.string.current_balance))
+                    Text(
+                        text = "${sign(currentBalance)}$currency${currentBalance.absoluteValue}",
+                        color = MaterialTheme.colors.primary
+                    )
+                })
+
+                Spacer(modifier = Modifier.size(16.dp))
+
+                BorderRow(content = listOf(
+                    {
+                        Text(text = incomeStr)
+                        Text(text = "$currency$income")
+                    },
+                    { Spacer(modifier = Modifier.size(12.dp)) },
+                    {
+                        Text(text = expenseStr)
+                        Text(text = "-$currency$expense")
                     }
-                }
-            )
-        },
-        backgroundColor = MaterialTheme.colors.surface
-    ) { padding ->
-        val income = stringResource(id = R.string.income)
-        val expense = stringResource(id = R.string.expense)
-        val currency = uiState.currency
-        fun sign(value: Double) = if (value >= 0) "+" else "-"
-        Column(modifier = Modifier.padding(padding)) {
-            Row(modifier = Modifier.padding(horizontal = 16.dp)) {
-                Column {
-                    Spacer(modifier = Modifier.size(24.dp))
-                    MonoDateRange(
-                        currentDate = uiState.currentData,
-                        onPrevious = { onEvent(MonthReportEvent.PreviousMonth) },
-                        onNext = { onEvent(MonthReportEvent.NextMonth) })
-                    Spacer(modifier = Modifier.size(24.dp))
+                ))
 
-                    BorderRow(content = listOf {
-                        Text(
-                            text = stringResource(id = R.string.current_balance)
-                        )
-                        Text(
-                            text = "${sign(uiState.currentBalance)}$currency${uiState.currentBalance.absoluteValue}",
-                            color = MaterialTheme.colors.primary
-                        )
-                    })
-                    Spacer(modifier = Modifier.size(16.dp))
+                Spacer(modifier = Modifier.size(16.dp))
 
-                    BorderRow(content = listOf(
-                        {
-                            Text(text = income)
-                            Text(text = "$currency${uiState.income}")
-                        },
-                        { Spacer(modifier = Modifier.size(12.dp)) },
-                        {
-                            Text(text = expense)
-                            Text(text = "-$currency${uiState.expense}")
-                        }
-                    ))
-                    Spacer(modifier = Modifier.size(16.dp))
+                BorderRow(content = listOf(
+                    {
+                        Text(text = "$expenseStr/$incomeStr")
+                        Text(text = "${sign(expenseIncome)}$currency${expenseIncome.absoluteValue}")
+                    },
+                    { Spacer(modifier = Modifier.size(12.dp)) },
+                    {
+                        Text(text = stringResource(id = R.string.previous_balance))
+                        Text(text = "$currency${previousBalance}")
+                    }
+                ))
 
-                    BorderRow(content = listOf(
-                        {
-                            Text(
-                                text = "$expense/$income"
-                            )
-                            Text(text = "${sign(uiState.expenseIncome)}$currency${uiState.expenseIncome.absoluteValue}")
-                        },
-                        { Spacer(modifier = Modifier.size(12.dp)) },
-                        {
-                            Text(text = stringResource(id = R.string.previous_balance))
-                            Text(text = "$currency${uiState.previousBalance}")
-                        }
-                    ))
+                Spacer(modifier = Modifier.size(24.dp))
 
-                    Spacer(modifier = Modifier.size(24.dp))
-
-                    MonoTabs(
-                        state = uiState.currentTab,
-                        titles = listOf(
-                            stringResource(id = R.string.all),
-                            stringResource(id = R.string.expense),
-                            stringResource(id = R.string.income)
-                        ),
-                        onSelect = { onEvent(MonthReportEvent.SelectTab(it)) })
-                }
+                MonoTabs(
+                    state = currentTab,
+                    titles = listOf(
+                        stringResource(id = R.string.all),
+                        stringResource(id = R.string.expense),
+                        stringResource(id = R.string.income)
+                    ),
+                    onSelect = { onEvent(ReportsEvent.SelectTab(it)) })
             }
-            MonoTransactionList(transactionList = uiState.transactionList, currency = currency)
         }
+        MonoTransactionList(transactionList = transactionList, currency = currency)
     }
 }
 
@@ -134,34 +108,32 @@ fun MonthReportScreen(
 fun PreviewMonthReportScreen() {
     MonoApplicationTheme {
         MonthReportScreen(
-            uiState = MonthReportUiState(
-                currentData = "February, 2022",
-                currency = "$",
-                currentBalance = 40710.00,
-                income = 143100.00,
-                expense = 118150.00,
-                expenseIncome = 24950.00,
-                previousBalance = 15760.00,
-                currentTab = 0,
-                transactionList = listOf(
-                    TransactionUi(
-                        id = 0,
-                        timestamp = 16165163564,
-                        amount = 10,
-                        note = "Note dfhgdfgdfdfgdsfdfgsdagfdfdfggdfgdfgdfgdfgdhfddfghhfddfgdfgh",
-                        type = TypeTransaction.Expense,
-                        category = "Food",
-                        icon = "category_food"
-                    ),
-                    TransactionUi(
-                        id = 1,
-                        timestamp = 16165165465,
-                        amount = 10,
-                        note = "Note",
-                        type = TypeTransaction.Income,
-                        category = "Pay",
-                        icon = "category_baby"
-                    )
+            currentData = "February, 2022",
+            currency = "$",
+            currentBalance = 40710.00,
+            income = 143100.00,
+            expense = 118150.00,
+            expenseIncome = 24950.00,
+            previousBalance = 15760.00,
+            currentTab = 0,
+            transactionList = listOf(
+                TransactionUi(
+                    id = 0,
+                    timestamp = 16165163564,
+                    amount = 10,
+                    note = "Note dfhgdfgdfdfgdsfdfgsdagfdfdfggdfgdfgdfgdfgdhfddfghhfddfgdfgh",
+                    type = TypeTransaction.Expense,
+                    category = "Food",
+                    icon = "category_food"
+                ),
+                TransactionUi(
+                    id = 1,
+                    timestamp = 16165165465,
+                    amount = 10,
+                    note = "Note",
+                    type = TypeTransaction.Income,
+                    category = "Pay",
+                    icon = "category_baby"
                 )
             )
         )
