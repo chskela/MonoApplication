48ddfcca60a63e7005b81d1b654775cfbb19c700
Chistaykov Aleksey <chskela@gmail.com>
2022-05-01 17:59:54 +0300

    Update ReportsUiState - add val isVisibleModal: Boolean = false, val title: Int = 0,

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/reports/models/ReportsUiState.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/reports/models/ReportsUiState.kt
@@ -5,6 +5,8 @@ import com.chskela.monoapplication.presentation.ui.components.categorysurface.Ca
 
 data class ReportsUiState(
     val report: Report = Report.Month,
+    val isVisibleModal: Boolean = false,
+    val title: Int = 0,
 //    MonthReportUiState
     val currentData: String = "February, 2022",
     val currency: String = "",
@@ -24,20 +26,3 @@ enum class Report(val info: String) {
     Category("Category"),
     Month("Month")
 }
-
-//data class CategoryReportUiState(
-//    val expenseList: List<CategoryUi> = emptyList(),
-//    val incomeList: List<CategoryUi> = emptyList()
-//)
-
-//data class MonthReportUiState(
-//    val currentData: String = "February, 2022",
-//    val currency: String = "",
-//    val currentBalance: Double = 0.0,
-//    val income: Double = 0.0,
-//    val expense: Double = 0.0,
-//    val expenseIncome: Double = 0.0,
-//    val previousBalance: Double = 0.0,
-//    val currentTab: Int = 0,
-//    val transactionList: List<TransactionUi> = emptyList()
-//)
