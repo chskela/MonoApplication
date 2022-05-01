05fc2a9721d8fbc4e8760699c02615348bf23334
Chistaykov Aleksey <chskela@gmail.com>
2022-05-01 18:09:22 +0300

    Update CategoryReportScreen - remove uiState

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/CategoryReportScreen.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/categoryreport/CategoryReportScreen.kt
@@ -4,83 +4,45 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.chskela.monoapplication.R
-import com.chskela.monoapplication.presentation.screens.categoryreport.models.CategoryReportUiState
+import com.chskela.monoapplication.presentation.ui.components.categorysurface.CategoryUi
 import com.chskela.monoapplication.presentation.ui.components.categorysurface.MonoCategorySurface
-import com.chskela.monoapplication.presentation.ui.components.topappbar.MonoTopAppBar
 import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme
 
 @Composable
-fun CategoryReportActivityScreen(
-    categoryReportViewModels: CategoryReportViewModels = hiltViewModel(),
-    onSelectCategory: (Long) -> Unit
-) {
-    CategoryReportScreen(
-        uiState = categoryReportViewModels.uiState.value,
-        onSelectCategory = onSelectCategory
-    )
-}
-
-@Composable
 fun CategoryReportScreen(
-    uiState: CategoryReportUiState,
+    expenseList: List<CategoryUi> = emptyList(),
+    incomeList: List<CategoryUi> = emptyList(),
     onSelectCategory: (Long) -> Unit = {}
 ) {
     val scrollState = rememberScrollState()
-    Scaffold(
-        topBar = {
-            MonoTopAppBar(
-                title = stringResource(id = R.string.category_report),
-                navigationIcon = {
-                    Spacer(modifier = Modifier.size(48.dp))
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = ImageVector.vectorResource(id = R.drawable.caret_down),
-                            tint = MaterialTheme.colors.secondary,
-                            contentDescription = "caret down"
-                        )
-                    }
-                }
-            )
-        },
-        backgroundColor = MaterialTheme.colors.surface
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .padding(padding)
-                .padding(horizontal = 16.dp)
-                .verticalScroll(scrollState),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
-        ) {
-            MonoCategorySurface(
-                listCategoryUi = uiState.expenseList,
-                title = stringResource(id = R.string.expense),
-                onClickItem = onSelectCategory,
-            )
-            Spacer(modifier = Modifier.height(40.dp))
-            MonoCategorySurface(
-                listCategoryUi = uiState.incomeList,
-                title = stringResource(id = R.string.income),
-                onClickItem = onSelectCategory,
-            )
-            Spacer(modifier = Modifier.height(40.dp))
-        }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        MonoCategorySurface(
+            listCategoryUi = expenseList,
+            title = stringResource(id = R.string.expense),
+            onClickItem = onSelectCategory,
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        MonoCategorySurface(
+            listCategoryUi = incomeList,
+            title = stringResource(id = R.string.income),
+            onClickItem = onSelectCategory,
+        )
+        Spacer(modifier = Modifier.height(40.dp))
     }
 }
 
@@ -89,8 +51,6 @@ fun CategoryReportScreen(
 @Composable
 fun PreviewCategoryReportScreen() {
     MonoApplicationTheme {
-        CategoryReportScreen(
-            uiState = CategoryReportUiState()
-        )
+        CategoryReportScreen()
     }
 }
