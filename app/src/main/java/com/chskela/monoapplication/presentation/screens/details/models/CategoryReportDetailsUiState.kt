f588fd5ed415dad6e0c16c842ff09ba6f69aa382
Chistaykov Aleksey <chskela@gmail.com>
2022-05-02 22:14:48 +0300

    Update CategoryReportDetailsUiState  -  add sumThisMonth

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/details/models/CategoryReportDetailsUiState.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/details/models/CategoryReportDetailsUiState.kt
@@ -2,11 +2,11 @@ package com.chskela.monoapplication.presentation.screens.details.models
 
 import com.chskela.monoapplication.domain.category.models.TypeCategory
 import com.chskela.monoapplication.presentation.screens.details.components.ReportUi
-import com.chskela.monoapplication.presentation.screens.monthreport.models.TransactionUi
 
 data class CategoryReportDetailsUiState(
     val currentTab: Int = 0,
     val currentCategory: Long = 0,
+    val sumThisMonth: Long = 0,
     val categoryName: String = "",
     val icon: String = "",
     val typeCategory: TypeCategory = TypeCategory.Expense,
