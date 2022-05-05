393b876eae7ed28393eb9aef669a9b9c106a1bb4
Chistaykov Aleksey <chskela@gmail.com>
2022-05-05 22:28:51 +0300

    Added GetAllTransactionsByMonthAndCategoryUseCase

---
+++ app/src/main/java/com/chskela/monoapplication/domain/reports/usecase/GetAllTransactionsByMonthAndCategoryUseCase.kt
@@ -0,0 +1,21 @@
+package com.chskela.monoapplication.domain.reports.usecase
+
+import com.chskela.monoapplication.domain.reports.repository.ReportsRepository
+import com.chskela.monoapplication.domain.transaction.models.Transaction
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.util.*
+
+class GetAllTransactionsByMonthAndCategoryUseCase(private val reportsRepository: ReportsRepository) {
+
+    operator fun invoke(categoryId: Long, month: Int): Flow<List<Transaction>> {
+            val calendar = Calendar.getInstance()
+            return reportsRepository.getAllTransactionsByCategory(categoryId).map { list ->
+                list.filter {
+                    calendar.timeInMillis = it.timestamp
+                    calendar.get(Calendar.MONTH) == month
+                }
+            }
+        }
+
+}
