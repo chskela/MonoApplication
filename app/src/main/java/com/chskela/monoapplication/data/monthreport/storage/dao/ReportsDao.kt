7caff35c89c1fc112db88bacce5a70bfc3f44f74
Chistaykov Aleksey <chskela@gmail.com>
2022-05-05 22:21:48 +0300

    Update ReportsDao  -  add getAllTransactionsByCategory

--- app/src/main/java/com/chskela/monoapplication/data/monthreport/storage/dao/ReportsDao.kt
+++ app/src/main/java/com/chskela/monoapplication/data/monthreport/storage/dao/ReportsDao.kt
@@ -3,6 +3,7 @@ package com.chskela.monoapplication.data.monthreport.storage.dao
 import androidx.room.Dao
 import androidx.room.Query
 import com.chskela.monoapplication.data.monthreport.storage.models.TransactionEntityWithCategory
+import com.chskela.monoapplication.data.transaction.storage.models.TransactionEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -10,4 +11,7 @@ interface ReportsDao {
 
     @Query("SELECT T.id, T.timestamp, T.amount, T.note, T.category_id, C.name, C.icon, C.type  FROM 'transaction' T JOIN category C ON T.category_id=C.id")
     fun getAllTransactions(): Flow<List<TransactionEntityWithCategory>>
+
+    @Query("SELECT T.id, T.timestamp, T.amount, T.note, T.category_id FROM 'transaction' T WHERE T.category_id = :categoryId")
+    fun getAllTransactionsByCategory(categoryId: Long): Flow<List<TransactionEntity>>
 }
