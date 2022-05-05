10e55301db42263fe18f55aa72a232e630a6bce9
Chistaykov Aleksey <chskela@gmail.com>
2022-05-05 21:50:30 +0300

    Update ReportsRepository  -  add getAllTransactionsByCategory

--- app/src/main/java/com/chskela/monoapplication/domain/reports/repository/ReportsRepository.kt
+++ app/src/main/java/com/chskela/monoapplication/domain/reports/repository/ReportsRepository.kt
@@ -5,4 +5,6 @@ import kotlinx.coroutines.flow.Flow
 
 interface ReportsRepository {
     fun getAllTransactions(): Flow<List<TransactionWithCategory>>
+
+    fun getAllTransactionsByCategory(): Flow<List<TransactionWithCategory>>
 }
