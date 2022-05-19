a6d28467b04148cfc975cd300ab6c4ce8be4d963
Chistaykov Aleksey <chskela@gmail.com>
2022-05-19 20:40:51 +0300

    Update AddEditCategoryViewModel - add init

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/add_edit_category/AddEditCategoryViewModel.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/add_edit_category/AddEditCategoryViewModel.kt
@@ -1,22 +1,27 @@
-package com.chskela.monoapplication.presentation.screens.edit_category
+package com.chskela.monoapplication.presentation.screens.add_edit_category
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chskela.monoapplication.data.icons.iconsMap
 import com.chskela.monoapplication.domain.category.models.Category
 import com.chskela.monoapplication.domain.category.models.TypeCategory
 import com.chskela.monoapplication.domain.category.usecase.CategoryUseCases
-import com.chskela.monoapplication.presentation.screens.edit_category.models.EditCategoryUiState
+import com.chskela.monoapplication.presentation.screens.add_edit_category.models.AddEditCategoryUiState
 import com.chskela.monoapplication.presentation.ui.components.categorysurface.CategoryUi
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class EditCategoryViewModel @Inject constructor(private val categoryUseCases: CategoryUseCases) :
+class AddEditCategoryViewModel @Inject constructor(
+    private val categoryUseCases: CategoryUseCases,
+    savedStateHandle: SavedStateHandle
+) :
     ViewModel() {
+
     private val icons = iconsMap.toList()
         .mapIndexed { index, icon ->
             CategoryUi(
@@ -25,16 +30,25 @@ class EditCategoryViewModel @Inject constructor(private val categoryUseCases: Ca
             )
         }
 
-    var uiState: MutableState<EditCategoryUiState> = mutableStateOf(
-        EditCategoryUiState(
+    var uiState: MutableState<AddEditCategoryUiState> = mutableStateOf(
+        AddEditCategoryUiState(
             icons = icons
         )
     )
         private set
 
-    fun onEvent(eventEdit: EditCategoryEvent) {
+    init {
+        savedStateHandle.get<Long>("categoryId")?.let { categoryId ->
+            if (categoryId != -1L) {
+                onEvent(AddEditCategoryEvent.GetCategory(categoryId))
+            }
+        }
+    }
+
+
+    fun onEvent(eventEdit: AddEditCategoryEvent) {
         when (eventEdit) {
-            is EditCategoryEvent.SelectTab -> {
+            is AddEditCategoryEvent.SelectTab -> {
                 val typeCategory =
                     if (eventEdit.tab == 0) TypeCategory.Expense else TypeCategory.Income
                 uiState.value = uiState.value.copy(
@@ -43,18 +57,18 @@ class EditCategoryViewModel @Inject constructor(private val categoryUseCases: Ca
                 )
             }
 
-            is EditCategoryEvent.ChangeCategoryName -> {
+            is AddEditCategoryEvent.ChangeCategoryName -> {
                 uiState.value = uiState.value.copy(categoryName = eventEdit.value)
             }
 
-            is EditCategoryEvent.ChangeCategoryIcon -> {
+            is AddEditCategoryEvent.ChangeCategoryIcon -> {
                 val icon = icons[eventEdit.iconId.toInt()].icon
                 icon?.let {
                     uiState.value = uiState.value.copy(icon = icon)
                 }
             }
 
-            is EditCategoryEvent.GetCategory -> {
+            is AddEditCategoryEvent.GetCategory -> {
                 uiState.value = uiState.value.copy(isNewCategory = false)
                 viewModelScope.launch {
                     val category = categoryUseCases.getCategoryByIdUseCase(eventEdit.categoryId)
@@ -67,7 +81,7 @@ class EditCategoryViewModel @Inject constructor(private val categoryUseCases: Ca
                 }
             }
 
-            is EditCategoryEvent.EditCategory -> {
+            is AddEditCategoryEvent.UpdateCategory -> {
                 viewModelScope.launch {
                     categoryUseCases.updateCategoryUseCase(
                         Category(
@@ -80,7 +94,7 @@ class EditCategoryViewModel @Inject constructor(private val categoryUseCases: Ca
                 }
             }
 
-            is EditCategoryEvent.AddCategory -> {
+            is AddEditCategoryEvent.AddCategory -> {
                 viewModelScope.launch {
                     categoryUseCases.addCategoryUseCase(
                         Category(
