626d8a03fba009a0e3f5815e6789e7354e910518
Chistaykov Aleksey <chskela@gmail.com>
2022-05-19 20:41:34 +0300

    Update EditCategoryScreen - remove EditCategoryActivityScreen, update onEvent in actions

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/add_edit_category/AddEditCategoryScreen.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/add_edit_category/AddEditCategoryScreen.kt
@@ -1,4 +1,4 @@
-package com.chskela.monoapplication.presentation.screens.edit_category
+package com.chskela.monoapplication.presentation.screens.add_edit_category
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
@@ -9,15 +9,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.chskela.monoapplication.R
-import com.chskela.monoapplication.presentation.screens.edit_category.models.EditCategoryUiState
+import com.chskela.monoapplication.presentation.screens.add_edit_category.models.AddEditCategoryUiState
 import com.chskela.monoapplication.presentation.ui.components.categorysurface.CategoryUi
 import com.chskela.monoapplication.presentation.ui.components.categorysurface.MonoCategorySurface
 import com.chskela.monoapplication.presentation.ui.components.tabs.MonoTabs
@@ -26,38 +24,9 @@ import com.chskela.monoapplication.presentation.ui.components.topappbar.MonoTopA
 import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme
 
 @Composable
-fun EditCategoryActivityScreen(
-    editCategoryViewModel: EditCategoryViewModel = hiltViewModel(),
-    onBack: () -> Unit,
-    categoryId: Long,
-) {
-    LaunchedEffect(true) {
-        editCategoryViewModel.onEvent(EditCategoryEvent.GetCategory(categoryId))
-    }
-
-    EditCategoryScreen(
-        uiState = editCategoryViewModel.uiState.value,
-        onEvent = editCategoryViewModel::onEvent,
-        onBack = onBack
-    )
-}
-
-@Composable
-fun AddCategoryActivityScreen(
-    editCategoryViewModel: EditCategoryViewModel = hiltViewModel(),
-    onBack: () -> Unit,
-) {
-    EditCategoryScreen(
-        uiState = editCategoryViewModel.uiState.value,
-        onEvent = editCategoryViewModel::onEvent,
-        onBack = onBack
-    )
-}
-
-@Composable
 fun EditCategoryScreen(
-    uiState: EditCategoryUiState,
-    onEvent: (EditCategoryEvent) -> Unit = {},
+    uiState: AddEditCategoryUiState,
+    onEvent: (AddEditCategoryEvent) -> Unit = {},
     onBack: () -> Unit = {}
 ) {
     val scrollState = rememberScrollState()
@@ -72,7 +41,13 @@ fun EditCategoryScreen(
                 title = stringResource(id = titleId),
                 actions = {
                     IconButton(onClick = {
-                        onEvent(EditCategoryEvent.AddCategory)
+                        onEvent(
+                            if (uiState.isNewCategory) {
+                                AddEditCategoryEvent.AddCategory
+                            } else {
+                                AddEditCategoryEvent.UpdateCategory
+                            }
+                        )
                         onBack()
                     }) {
                         Text(
@@ -97,20 +72,20 @@ fun EditCategoryScreen(
             MonoTabs(
                 state = uiState.currentTab,
                 titles = titles,
-                onSelect = { onEvent(EditCategoryEvent.SelectTab(it)) }
+                onSelect = { onEvent(AddEditCategoryEvent.SelectTab(it)) }
             )
             Spacer(modifier = Modifier.height(24.dp))
             MonoTextField(
                 label = stringResource(id = R.string.category_name),
                 value = uiState.categoryName,
-                onValueChange = { onEvent(EditCategoryEvent.ChangeCategoryName(it)) }
+                onValueChange = { onEvent(AddEditCategoryEvent.ChangeCategoryName(it)) }
             )
             Spacer(modifier = Modifier.height(40.dp))
             MonoCategorySurface(
                 listCategoryUi = uiState.icons,
                 title = stringResource(id = R.string.icon),
                 selectedCategory = uiState.icons.firstOrNull { it.icon == uiState.icon }?.id ?: -1,
-                onClickItem = { onEvent(EditCategoryEvent.ChangeCategoryIcon(it)) }
+                onClickItem = { onEvent(AddEditCategoryEvent.ChangeCategoryIcon(it)) }
             )
             Spacer(modifier = Modifier.height(40.dp))
         }
@@ -123,7 +98,7 @@ fun EditCategoryScreen(
 fun PreviewEditCategoryScreen() {
     MonoApplicationTheme {
         EditCategoryScreen(
-            uiState = EditCategoryUiState(
+            uiState = AddEditCategoryUiState(
                 icons = listOf(
                     CategoryUi(
                         id = 0,
