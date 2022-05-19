f3014830445a124b059e38a114375a6981caa42b
Chistaykov Aleksey <chskela@gmail.com>
2022-05-19 21:04:13 +0300

    Update reportGraph - refactor onSelectCategory, update composable(MonoScreens.CategoryReportDetails.name)

--- app/src/main/java/com/chskela/monoapplication/presentation/navigation/graphs/reportGraph.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/navigation/graphs/reportGraph.kt
@@ -1,12 +1,16 @@
 package com.chskela.monoapplication.presentation.navigation.graphs
 
+import android.util.Log
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import androidx.navigation.navArgument
 import com.chskela.monoapplication.presentation.navigation.MonoScreens
-import com.chskela.monoapplication.presentation.screens.details.CategoryReportDetailsActivityScreen
+import com.chskela.monoapplication.presentation.screens.details.CategoryReportDetailsScreen
+import com.chskela.monoapplication.presentation.screens.details.CategoryReportDetailsViewModels
 import com.chskela.monoapplication.presentation.screens.reports.ReportsScreen
 import com.chskela.monoapplication.presentation.screens.reports.ReportsViewModels
 
@@ -17,31 +21,31 @@ fun NavGraphBuilder.reportGraph(navController: NavController) {
     ) {
 
         composable(MonoScreens.Reports.name) {
-            fun onClick(id: Long) {
-                navController.currentBackStackEntry?.arguments?.putLong(
-                    "categoryId",
-                    id
-                )
-                navController.navigate(MonoScreens.CategoryReportDetails.name)
-            }
-
             val reportViewModels: ReportsViewModels = hiltViewModel()
             ReportsScreen(
                 uiState = reportViewModels.uiState.value,
                 onEvent = reportViewModels::onEvent,
-                onSelectCategory = ::onClick
+                onSelectCategory = { id ->
+                    navController
+                        .navigate("${MonoScreens.CategoryReportDetails.name}?categoryId=${id}")
+                }
             )
         }
 
-        composable(MonoScreens.CategoryReportDetails.name) {
-            val categoryId =
-                navController.previousBackStackEntry?.arguments?.getLong("categoryId")
-            categoryId?.let {
-                CategoryReportDetailsActivityScreen(
-                    onBack = { navController.navigateUp() },
-                    categoryId = it
-                )
-            }
+        composable(
+            route = "${MonoScreens.CategoryReportDetails.name}?categoryId={categoryId}",
+            arguments = listOf(navArgument(name = "categoryId") {
+                type = NavType.LongType
+                defaultValue = -1
+            })
+
+        ) {
+            val categoryReportDetailsViewModels: CategoryReportDetailsViewModels = hiltViewModel()
+            CategoryReportDetailsScreen(
+                uiState = categoryReportDetailsViewModels.uiState.value,
+                onEvent = categoryReportDetailsViewModels::onEvent,
+                onBack = { navController.navigateUp() })
+
         }
     }
 }
