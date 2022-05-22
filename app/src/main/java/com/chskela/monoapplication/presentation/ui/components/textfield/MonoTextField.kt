83391070887b2568cd675673c9166498c2b18b34
chskela <chskela@gmail.com>
2022-05-22 21:09:50 +0300

    Update MonoTextField - add keyboard hide for onDone

--- app/src/main/java/com/chskela/monoapplication/presentation/ui/components/textfield/MonoTextField.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/ui/components/textfield/MonoTextField.kt
@@ -3,15 +3,19 @@ package com.chskela.monoapplication.presentation.ui.components.textfield
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MonoTextField(
     label: String = "",
@@ -25,6 +29,8 @@ fun MonoTextField(
     leadingIcon: @Composable (() -> Unit)? = null,
     trailingIcon: @Composable (() -> Unit)? = null,
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Text(text = label,
         style = MaterialTheme.typography.h6,
         color = MaterialTheme.colors.secondary)
@@ -40,6 +46,9 @@ fun MonoTextField(
         maxLines = maxLines,
         value = value,
         onValueChange = { onValueChange(it) },
-        keyboardOptions = keyboardOptions
+        keyboardOptions = keyboardOptions,
+        keyboardActions = KeyboardActions(
+            onDone = { keyboardController?.hide() }
+        ),
     )
 }
