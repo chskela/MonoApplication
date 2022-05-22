e0ab5d41a3fbc7a8ec211ad6bbb31007966ee3ce
Chistaykov Aleksey <chskela@gmail.com>
2022-05-22 13:29:03 +0300

    Update GetCategoryByIdUseCase - return Flow<Category>

--- app/src/main/java/com/chskela/monoapplication/domain/category/usecase/GetCategoryByIdUseCase.kt
+++ app/src/main/java/com/chskela/monoapplication/domain/category/usecase/GetCategoryByIdUseCase.kt
@@ -2,10 +2,11 @@ package com.chskela.monoapplication.domain.category.usecase
 
 import com.chskela.monoapplication.domain.category.models.Category
 import com.chskela.monoapplication.domain.category.repository.CategoryRepository
+import kotlinx.coroutines.flow.Flow
 
 class GetCategoryByIdUseCase (private val categoryRepository: CategoryRepository) {
 
-     suspend operator fun invoke(id: Long) : Category {
+     operator fun invoke(id: Long) : Flow<Category> {
         return categoryRepository.getCategoryById(id)
     }
 }
