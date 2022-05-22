af5e0f0ae56ad2294d7ee8bd8d2b91d07ca77bc2
chskela <chskela@gmail.com>
2022-05-22 18:46:21 +0300

    Update CategoryScreen - add snackbar, fun onDeleteCategory

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/category/CategoryScreen.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/category/CategoryScreen.kt
@@ -1,6 +1,7 @@
 package com.chskela.monoapplication.presentation.screens.category
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -8,34 +9,20 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.chskela.monoapplication.R
 import com.chskela.monoapplication.presentation.screens.category.models.CategoryUiState
 import com.chskela.monoapplication.presentation.ui.components.categorysurface.CategoryUi
 import com.chskela.monoapplication.presentation.ui.components.categorysurface.MonoCategorySurface
 import com.chskela.monoapplication.presentation.ui.components.topappbar.MonoTopAppBar
 import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme
-
-@Composable
-fun CategoryActivityScreen(
-    categoryViewModel: CategoryViewModel = hiltViewModel(),
-    onBack: () -> Unit,
-    onClick: (Long) -> Unit,
-    onAddMore: () -> Unit
-) {
-    CategoryScreen(
-        uiState = categoryViewModel.uiState.value,
-        onEvent = categoryViewModel::onEvent,
-        onBack = onBack,
-        onEdit = onClick,
-        onAddMore = onAddMore
-    )
-}
+import kotlinx.coroutines.launch
 
 @Composable
 fun CategoryScreen(
@@ -46,8 +33,54 @@ fun CategoryScreen(
     onAddMore: () -> Unit = {}
 ) {
     val scrollState = rememberScrollState()
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    fun onDeleteCategory(categoryId: Long) {
+        onEvent(CategoryEvent.DeleteCategory(categoryId))
+        scope.launch {
+            val result = scaffoldState.snackbarHostState.showSnackbar(
+                message = "Category deleted",
+                actionLabel = "Undo"
+            )
+            if (result == SnackbarResult.ActionPerformed) {
+                onEvent(CategoryEvent.Restore)
+            }
+        }
+    }
 
     Scaffold(
+        scaffoldState = scaffoldState,
+        snackbarHost = {
+            SnackbarHost(hostState = it) { data ->
+                Snackbar(
+                    modifier = Modifier.padding(16.dp),
+                    shape = MaterialTheme.shapes.large,
+                    backgroundColor = MaterialTheme.colors.background,
+                    action = {
+                        data.actionLabel?.let { it ->
+                            Text(
+                                modifier = Modifier
+                                    .clickable {
+                                        data.performAction()
+                                    }
+                                    .padding(8.dp),
+                                text = it.uppercase(),
+                                style = MaterialTheme.typography.body1
+                                    .copy(fontWeight = FontWeight.Bold),
+                                color = MaterialTheme.colors.primary
+                            )
+                        }
+                    }
+                ) {
+                    Text(
+                        text = data.message,
+                        style = MaterialTheme.typography.body1,
+                        color = MaterialTheme.colors.onBackground
+                    )
+                }
+            }
+        },
         topBar = {
             MonoTopAppBar(
                 title = stringResource(id = R.string.category), onNavigation = onBack,
@@ -72,13 +105,13 @@ fun CategoryScreen(
                 listCategoryUi = uiState.expenseList,
                 title = stringResource(id = R.string.expense),
                 onClickItem = onEdit,
-                onLongClick = {onEvent(CategoryEvent.DeleteCategory(it))}
+                onLongClick = ::onDeleteCategory
             )
             MonoCategorySurface(
                 listCategoryUi = uiState.incomeList,
                 title = stringResource(id = R.string.income),
                 onClickItem = onEdit,
-                onLongClick = {onEvent(CategoryEvent.DeleteCategory(it))}
+                onLongClick = ::onDeleteCategory
             )
         }
     }
