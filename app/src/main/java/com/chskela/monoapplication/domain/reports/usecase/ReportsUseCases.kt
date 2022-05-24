90ac8a0efe3dce85c121f21a5ea88508c23530ff
chskela <chskela@gmail.com>
2022-05-24 22:47:43 +0300

    Delete ReportsUseCases

--- app/src/main/java/com/chskela/monoapplication/domain/reports/usecase/ReportsUseCases.kt
+++
@@ -1,6 +0,0 @@
-package com.chskela.monoapplication.domain.reports.usecase
-
-data class ReportsUseCases(
-    val getAllTransactionsByMonthUseCase: GetAllTransactionsByMonthUseCase,
-    val getAllTransactionsUseCase: GetAllTransactionsUseCase
-)
