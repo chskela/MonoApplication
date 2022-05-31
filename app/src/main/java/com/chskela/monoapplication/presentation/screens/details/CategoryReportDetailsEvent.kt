128fd3f0823b0b8400272020842b91bde097c98a
chskela <chskela@gmail.com>
2022-05-31 21:28:00 +0300

    Update CategoryReportDetailsEvent - rename GetCategory to GetData

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/details/CategoryReportDetailsEvent.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/details/CategoryReportDetailsEvent.kt
@@ -2,5 +2,5 @@ package com.chskela.monoapplication.presentation.screens.details
 
 sealed class CategoryReportDetailsEvent{
     data class SelectTab(val tab: Int) : CategoryReportDetailsEvent()
-    data class GetCategory(val categoryId: Long) : CategoryReportDetailsEvent()
+    data class GetData(val categoryId: Long) : CategoryReportDetailsEvent()
 }
