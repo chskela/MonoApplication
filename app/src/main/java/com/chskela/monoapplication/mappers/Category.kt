37906224bcda152407c025e4cde3a9eeb747abc9
Chistyakov Alexey <chskela@gmail.com>
2022-10-30 19:10:03 +0300

    Updated Category mapper - create extension fun mapToCategoryEntity and mapToCategory

--- app/src/main/java/com/chskela/monoapplication/mappers/Category.kt
+++ app/src/main/java/com/chskela/monoapplication/mappers/Category.kt
@@ -1,6 +1,21 @@
 package com.chskela.monoapplication.mappers
 
+import com.chskela.monoapplication.data.category.storage.models.CategoryEntity
 import com.chskela.monoapplication.domain.category.models.Category
 import com.chskela.monoapplication.presentation.ui.components.categorysurface.CategoryUi
 
-fun Category.mapCategoryToUi() = CategoryUi(id = id, icon = icon, title = name)
+fun Category.mapToCategoryUi(): CategoryUi = CategoryUi(id = id, icon = icon, title = name)
+
+fun Category.mapToCategoryEntity(): CategoryEntity = CategoryEntity(
+    id = if (id == 0L) null else id,
+    name = name,
+    icon = icon,
+    type = type.mapToType()
+)
+
+fun CategoryEntity.mapToCategory(): Category = Category(
+    id = id ?: 0,
+    name = name,
+    icon = icon,
+    type = type.mapToTypeCategory()
+)
