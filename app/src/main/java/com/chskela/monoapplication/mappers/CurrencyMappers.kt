e0bc88d193f1d2743aa0034cb65bfd679402c69d
Chistyakov Alexey <chskela@gmail.com>
2022-10-31 21:01:52 +0300

    Created CurrencyMappers

---
+++ app/src/main/java/com/chskela/monoapplication/mappers/CurrencyMappers.kt
@@ -0,0 +1,18 @@
+package com.chskela.monoapplication.mappers
+
+import com.chskela.monoapplication.data.currency.storage.models.CurrencyEntity
+import com.chskela.monoapplication.domain.currency.models.Currency
+
+fun CurrencyEntity.mapToCurrency() = Currency(
+    id = id ?: 0,
+    name = name,
+    letterCode = letterCode,
+    symbol = symbol
+)
+
+fun Currency.mapToCurrencyEntity() = CurrencyEntity(
+    id = id,
+    name = name,
+    letterCode = letterCode,
+    symbol = symbol
+)
