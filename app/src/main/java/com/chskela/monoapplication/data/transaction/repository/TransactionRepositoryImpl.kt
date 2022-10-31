7cacd0a6990975837f60275374fc1ad69e11cb2d
Chistyakov Alexey <chskela@gmail.com>
2022-10-31 21:03:55 +0300

    Updated TransactionRepositoryImpl - refactor pass mapped extension fun

--- app/src/main/java/com/chskela/monoapplication/data/transaction/repository/TransactionRepositoryImpl.kt
+++ app/src/main/java/com/chskela/monoapplication/data/transaction/repository/TransactionRepositoryImpl.kt
@@ -1,10 +1,10 @@
 package com.chskela.monoapplication.data.transaction.repository
 
 import com.chskela.monoapplication.data.transaction.storage.dao.TransactionDao
-import com.chskela.monoapplication.data.transaction.storage.models.TransactionEntity
 import com.chskela.monoapplication.data.reports.storage.models.TransactionEntityWithCategory
 import com.chskela.monoapplication.domain.transaction.models.Transaction
 import com.chskela.monoapplication.domain.transaction.repository.TransactionRepository
+import com.chskela.monoapplication.mappers.mapToTransactionEntity
 
 class TransactionRepositoryImpl(private val transactionDao: TransactionDao) :
     TransactionRepository {
@@ -14,36 +14,18 @@ class TransactionRepositoryImpl(private val transactionDao: TransactionDao) :
     }
 
     override suspend fun insertTransaction(transaction: Transaction) {
-        transactionDao.insertTransaction(mapCategoryToStorage(transaction))
+        transactionDao.insertTransaction(transaction.mapToTransactionEntity())
     }
 
     override suspend fun updateTransaction(transaction: Transaction) {
-        transactionDao.updateTransaction(mapCategoryToStorage(transaction))
+        transactionDao.updateTransaction(transaction.mapToTransactionEntity())
     }
 
     override suspend fun deleteTransaction(transaction: Transaction) {
-        transactionDao.deleteTransaction(mapCategoryToStorage(transaction))
+        transactionDao.deleteTransaction(transaction.mapToTransactionEntity())
     }
 
     override suspend fun deleteAllTransactions() {
         transactionDao.deleteAllTransactions()
     }
-
-//    private fun mapTransactionToDomain(transactionEntityWithCategory: TransactionEntityWithCategory): Transaction =
-//        Transaction(
-//            id = transactionEntity.id ?: 0,
-//            timestamp = transactionEntity.timestamp,
-//            amount = transactionEntity.amount,
-//            note = transactionEntity.note,
-//            categoryId = transactionEntity.categoryId,
-//            currencyId = transactionEntity.currencyId
-//        )
-
-    private fun mapCategoryToStorage(transaction: Transaction): TransactionEntity =
-        TransactionEntity(
-            timestamp = transaction.timestamp,
-            amount = transaction.amount,
-            note = transaction.note,
-            categoryId = transaction.categoryId,
-        )
 }
