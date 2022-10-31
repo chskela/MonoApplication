5373a72732c94c7e92278f2843b7d54aaa79a4b1
Chistyakov Alexey <chskela@gmail.com>
2022-10-31 21:02:32 +0300

    Updated ReportsRepositoryImpl - refactor pass mapped extension fun

--- app/src/main/java/com/chskela/monoapplication/data/reports/repository/ReportsRepositoryImpl.kt
+++ app/src/main/java/com/chskela/monoapplication/data/reports/repository/ReportsRepositoryImpl.kt
@@ -1,13 +1,11 @@
 package com.chskela.monoapplication.data.reports.repository
 
-import com.chskela.monoapplication.data.category.storage.models.Type
 import com.chskela.monoapplication.data.reports.storage.dao.ReportsDao
-import com.chskela.monoapplication.data.reports.storage.models.TransactionEntityWithCategory
-import com.chskela.monoapplication.data.transaction.storage.models.TransactionEntity
-import com.chskela.monoapplication.domain.category.models.TypeCategory
 import com.chskela.monoapplication.domain.reports.models.TransactionWithCategory
 import com.chskela.monoapplication.domain.reports.repository.ReportsRepository
 import com.chskela.monoapplication.domain.transaction.models.Transaction
+import com.chskela.monoapplication.mappers.mapToTransaction
+import com.chskela.monoapplication.mappers.mapToTransactionWithCategory
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -16,40 +14,13 @@ class ReportsRepositoryImpl(private val reportsDao: ReportsDao) :
 
     override fun getAllTransactions(): Flow<List<TransactionWithCategory>> {
         return reportsDao.getAllTransactions().map { list ->
-            list.map { mapToDomain(it) }
+            list.map { it.mapToTransactionWithCategory() }
         }
     }
 
     override fun getAllTransactionsByCategory(categoryId: Long): Flow<List<Transaction>> {
         return reportsDao.getAllTransactionsByCategory(categoryId).map { list ->
-            list.map { mapTransactionToDomain(it) }
+            list.map { it.mapToTransaction() }
         }
     }
-
-    private fun mapTypeToDomain(type: Type): TypeCategory = when (type) {
-        Type.Expense -> TypeCategory.Expense
-        Type.Income -> TypeCategory.Income
-    }
-
-    private fun mapToDomain(transactionEntityWithCategory: TransactionEntityWithCategory): TransactionWithCategory =
-        TransactionWithCategory(
-            id = transactionEntityWithCategory.id ?: 0,
-            timestamp = transactionEntityWithCategory.timestamp,
-            amount = transactionEntityWithCategory.amount,
-            note = transactionEntityWithCategory.note,
-            categoryId = transactionEntityWithCategory.categoryId,
-            name = transactionEntityWithCategory.name,
-            icon = transactionEntityWithCategory.icon,
-            type = mapTypeToDomain(transactionEntityWithCategory.type)
-        )
-
-        private fun mapTransactionToDomain(transactionEntity: TransactionEntity): Transaction =
-        Transaction(
-            id = transactionEntity.id ?: 0,
-            timestamp = transactionEntity.timestamp,
-            amount = transactionEntity.amount,
-            note = transactionEntity.note,
-            categoryId = transactionEntity.categoryId,
-        )
-
 }
