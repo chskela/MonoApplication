fb4ca7a3f6fc4c3e7fe336e348ed63e12e95413e
Chistyakov Alexey <chskela@gmail.com>
2023-01-18 22:47:46 +0300

    Updated theme - add material3

--- app/src/main/java/com/chskela/monoapplication/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.chskela.monoapplication.presentation.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.material3.Typography as Typography3
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -59,3 +60,56 @@ val TypographyRoboto = Typography(
     )
 )
 
+
+// Set of Material typography styles to start with
+val Typography = Typography3(
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 36.sp,
+        textAlign = TextAlign.Center,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 14.sp,
+        textAlign = TextAlign.Center,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        textAlign = TextAlign.Center
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 56.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp
+    )
+)
