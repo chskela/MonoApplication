e13e9bc55d9657718cfea049935002a8fabab016
Chistyakov Alexey <chskela@gmail.com>
2023-01-19 14:50:49 +0300

    Updated MonoTabs on M3

--- app/src/main/java/com/chskela/monoapplication/presentation/ui/components/tabs/MonoTabs.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/ui/components/tabs/MonoTabs.kt
@@ -1,21 +1,24 @@
 package com.chskela.monoapplication.presentation.ui.components.tabs
 
+import android.content.res.Configuration
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.material.*
-import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.*
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme3
 
 @Composable
 fun MonoTabs(
     modifier: Modifier = Modifier,
     state: Int,
     titles: List<String>,
-    onSelect: (Int) -> Unit,
+    onSelect: (Int) -> Unit = {},
 ) {
     TabRow(
         modifier = modifier,
@@ -23,18 +26,18 @@ fun MonoTabs(
         indicator = @Composable { tabPositions ->
             TabRowDefaults.Indicator(
                 Modifier.tabIndicatorOffset(tabPositions[state]),
-                color = MaterialTheme.colors.primary
+                color = MaterialTheme.colorScheme.primary
             )
         },
-        backgroundColor = MaterialTheme.colors.surface
+        containerColor = MaterialTheme.colorScheme.surface
     ) {
         titles.forEachIndexed { index, title ->
             val selected = state == index
             val color: Color by animateColorAsState(
                 targetValue = if (selected) {
-                    MaterialTheme.colors.primary
+                    MaterialTheme.colorScheme.primary
                 } else {
-                    MaterialTheme.colors.secondaryVariant
+                    MaterialTheme.colorScheme.secondary
                 },
                 animationSpec = spring(
                     dampingRatio = Spring.DampingRatioHighBouncy,
@@ -44,7 +47,7 @@ fun MonoTabs(
             Tab(
                 text = {
                     Text(
-                        text = title, style = MaterialTheme.typography.body1,
+                        text = title, style = MaterialTheme.typography.bodyLarge,
                         color = color
                     )
                 },
@@ -52,5 +55,17 @@ fun MonoTabs(
                 onClick = { onSelect(index) }
             )
         }
+    }
+}
+
+@Preview(showBackground = true, name = "Light CategoryScreen")
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewCategoryItem() {
+    MonoApplicationTheme3 {
+        MonoTabs(
+            state = 0,
+            titles = listOf("test1", "test2")
+        )
     }
 }
