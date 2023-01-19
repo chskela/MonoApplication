56ddc90c8909cf66d6476c9a051d8eee206f3ee9
Chistyakov Alexey <chskela@gmail.com>
2023-01-19 14:54:06 +0300

    Updated MonoTextField on M3

--- app/src/main/java/com/chskela/monoapplication/presentation/ui/components/textfield/MonoTextField.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/ui/components/textfield/MonoTextField.kt
@@ -1,26 +1,30 @@
 package com.chskela.monoapplication.presentation.ui.components.textfield
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme3
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun MonoTextField(
     label: String = "",
-    value: String,
-    textStyle: TextStyle = MaterialTheme.typography.subtitle2,
+    value: String = "",
+    textStyle: TextStyle = MaterialTheme.typography.titleSmall,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     singleLine: Boolean = true,
     maxLines: Int = 5,
@@ -31,15 +35,17 @@ fun MonoTextField(
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
 
-    Text(text = label,
-        style = MaterialTheme.typography.h6,
-        color = MaterialTheme.colors.secondary)
+    Text(
+        text = label,
+        style = MaterialTheme.typography.titleLarge,
+        color = MaterialTheme.colorScheme.secondary
+    )
     Spacer(modifier = Modifier.height(8.dp))
     OutlinedTextField(
         modifier = Modifier.fillMaxWidth(),
         trailingIcon = trailingIcon,
         leadingIcon = leadingIcon,
-        textStyle = textStyle.copy(color = MaterialTheme.colors.onSurface),
+        textStyle = textStyle.copy(color = MaterialTheme.colorScheme.onSurface),
         shape = MaterialTheme.shapes.medium,
         placeholder = placeholder,
         singleLine = singleLine,
@@ -51,4 +57,15 @@ fun MonoTextField(
             onDone = { keyboardController?.hide() }
         ),
     )
+}
+
+@Preview(showBackground = true, name = "Light CategoryScreen")
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewCategoryItem() {
+    MonoApplicationTheme3 {
+        MonoTextField(
+            value = "value"
+        )
+    }
 }
