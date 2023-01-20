dbd18a1c2827037df01e93905aa85dcab16f01d6
Chistyakov Alexey <chskela@gmail.com>
2023-01-20 20:07:32 +0300

    Updated SettingsScreen on M3

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/settings/SettingsScreen.kt
@@ -2,8 +2,9 @@ package com.chskela.monoapplication.presentation.screens.settings
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -14,7 +15,7 @@ import com.chskela.monoapplication.R
 import com.chskela.monoapplication.presentation.screens.settings.components.SettingItem
 import com.chskela.monoapplication.presentation.screens.settings.models.SettingUiItem
 import com.chskela.monoapplication.presentation.ui.components.topappbar.MonoTopAppBar
-import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme
+import com.chskela.monoapplication.presentation.ui.theme.MonoApplicationTheme3
 
 @Composable
 fun SettingsActivityScreen(
@@ -29,6 +30,7 @@ fun SettingsActivityScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(
     onCategory: () -> Unit = {},
@@ -41,12 +43,12 @@ fun SettingsScreen(
                 Spacer(modifier = Modifier.size(48.dp))
             })
         },
-        backgroundColor = MaterialTheme.colors.surface
-    ) {padding ->
+        containerColor = MaterialTheme.colorScheme.surface
+    ) { padding ->
         Column(
             modifier = Modifier
                 .padding(padding)
-                .padding(top = 24.dp)
+                .padding(top = 18.dp)
                 .padding(horizontal = 16.dp)
         ) {
             SettingItem(
@@ -54,7 +56,7 @@ fun SettingsScreen(
                     title = stringResource(id = R.string.category),
                     leftIcon = R.drawable.settings_squares_four,
                     rightIcon = R.drawable.settings_caret_circle_right,
-                    color = MaterialTheme.colors.onSurface,
+                    color = MaterialTheme.colorScheme.onSurface,
                     onClick = { onCategory() }
                 ),
             )
@@ -64,7 +66,7 @@ fun SettingsScreen(
                     title = stringResource(id = R.string.currency),
                     leftIcon = R.drawable.settings_currency_circle_dollar,
                     rightIcon = R.drawable.settings_caret_circle_right,
-                    color = MaterialTheme.colors.onSurface,
+                    color = MaterialTheme.colorScheme.onSurface,
                     onClick = { onCurrency() }
                 ),
             )
@@ -73,7 +75,7 @@ fun SettingsScreen(
                 settingUiItem = SettingUiItem(
                     title = stringResource(id = R.string.delete_all_data),
                     leftIcon = R.drawable.settings_trash,
-                    color = MaterialTheme.colors.error,
+                    color = MaterialTheme.colorScheme.error,
                     onClick = deleteAllData
                 )
             )
@@ -85,7 +87,7 @@ fun SettingsScreen(
 @Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewSettingsScreen() {
-    MonoApplicationTheme {
+    MonoApplicationTheme3 {
         SettingsScreen()
     }
 }
