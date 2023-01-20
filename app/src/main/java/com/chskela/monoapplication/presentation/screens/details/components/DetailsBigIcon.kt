7483cf900c57c3443bf29fecf39350eabd0a3a29
Chistyakov Alexey <chskela@gmail.com>
2023-01-20 20:47:19 +0300

    Updated DetailsBigIcon on M3

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/details/components/DetailsBigIcon.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/details/components/DetailsBigIcon.kt
@@ -3,9 +3,9 @@ package com.chskela.monoapplication.presentation.screens.details.components
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,8 +48,8 @@ fun DetailsBigIcon(
 
             Text(
                 text = title,
-                style = MaterialTheme.typography.subtitle1,
-                color = MaterialTheme.colors.secondary
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.secondary
             )
         }
     }
