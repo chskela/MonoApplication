abf4ecf95e1fda5f9355b49ae1eaeffc5366007a
Chistyakov Alexey <chskela@gmail.com>
2023-01-22 01:46:29 +0300

    Updated Theme.kt - update statusBarColor

--- app/src/main/java/com/chskela/monoapplication/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/ui/theme/Theme.kt
@@ -97,7 +97,7 @@ fun MonoApplicationTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            (view.context as Activity).window.statusBarColor = colorScheme.secondaryContainer.toArgb()
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
         }
     }
