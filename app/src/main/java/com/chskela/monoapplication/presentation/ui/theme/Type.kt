49706d9f92aacc489f356a8b648be7cdbee57560
Chistyakov Alexey <chskela@gmail.com>
2023-01-21 17:25:40 +0300

    Updated Typography on M3

--- app/src/main/java/com/chskela/monoapplication/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/ui/theme/Type.kt
@@ -1,68 +1,15 @@
 package com.chskela.monoapplication.presentation.ui.theme
 
-import androidx.compose.material.Typography
-import androidx.compose.material3.Typography as Typography3
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
-val TypographyRoboto = Typography(
-    h1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 32.sp
-    ),
-    h2 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-        lineHeight = 36.sp,
-        textAlign = TextAlign.Center,
-    ),
-    h6 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontSize = 14.sp,
-        textAlign = TextAlign.Center,
-    ),
-    subtitle1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 20.sp
-    ),
-    subtitle2 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 16.sp
-    ),
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        textAlign = TextAlign.Center
-    ),
-    body2 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 56.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    ),
-    overline = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 10.sp
-    )
-)
-
 
 // Set of Material typography styles to start with
-val Typography = Typography3(
+val Typography = Typography(
     displayLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
