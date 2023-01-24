183b4e55c8919811e6359099ffe8e0f315904bc6
Chistyakov Alexey <chskela@gmail.com>
2023-01-24 14:13:10 +0300

    Updated AddAndEditCategoryUiState - rename properties

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/add_edit_category/models/AddAndEditCategoryUiState.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/add_edit_category/models/AddAndEditCategoryUiState.kt
@@ -4,10 +4,10 @@ import com.chskela.monoapplication.domain.category.models.TypeCategory
 import com.chskela.monoapplication.presentation.ui.components.categorysurface.CategoryUi
 
 data class AddAndEditCategoryUiState(
-    val id: Long = 0,
+    val categoryId: Long = 0,
     val currentTab: Int = 0,
     val categoryName: String = "",
-    val icon: String = "",
+    val currentIcon: String = "",
     val typeCategory: TypeCategory = TypeCategory.Expense,
     val icons: List<CategoryUi> = emptyList(),
     val isNewCategory: Boolean = true
