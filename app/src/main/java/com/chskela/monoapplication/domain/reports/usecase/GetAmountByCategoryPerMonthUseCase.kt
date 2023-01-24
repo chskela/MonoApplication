0e38db22ac18fa5090c50b452a3742e860a5592a
Chistyakov Alexey <chskela@gmail.com>
2023-01-24 21:52:04 +0300

    Added GetAmountByCategoryPerMonthUseCase

---
+++ app/src/main/java/com/chskela/monoapplication/domain/reports/usecase/GetAmountByCategoryPerMonthUseCase.kt
@@ -0,0 +1,22 @@
+package com.chskela.monoapplication.domain.reports.usecase
+
+import com.chskela.monoapplication.domain.reports.repository.ReportsRepository
+import kotlinx.coroutines.flow.Flow
+import java.util.Calendar.*
+
+class GetAmountByCategoryPerMonthUseCase(private val reportsRepository: ReportsRepository) {
+
+    operator fun invoke(categoryId: Long): Flow<Long> {
+        val calendar = getInstance()
+        calendar.set(DAY_OF_MONTH, 1)
+        calendar.set(HOUR_OF_DAY, 0)
+        calendar.set(MINUTE, 0)
+        calendar.set(SECOND, 0)
+        calendar.set(MILLISECOND, 0)
+        val startOfMonthInMilliseconds = calendar.timeInMillis
+        return reportsRepository.getAmountByCategoryPerMonthUseCase(
+            categoryId = categoryId,
+            startOfMonthInMilliseconds = startOfMonthInMilliseconds
+        )
+    }
+}
