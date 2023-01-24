02b4bae9ba1e04aec02d4a1b67d0d660862143bc
Chistyakov Alexey <chskela@gmail.com>
2023-01-24 21:54:12 +0300

    Updated ReportsRepositoryImpl - impl AbstractRepository and getAmountByCategoryPerMonth

--- app/src/main/java/com/chskela/monoapplication/data/reports/repository/ReportsRepositoryImpl.kt
+++ app/src/main/java/com/chskela/monoapplication/data/reports/repository/ReportsRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.chskela.monoapplication.data.reports.repository
 
 import com.chskela.monoapplication.data.reports.storage.dao.ReportsDao
+import com.chskela.monoapplication.domain.common.AbstractRepository
 import com.chskela.monoapplication.domain.reports.models.TransactionWithCategory
 import com.chskela.monoapplication.domain.reports.repository.ReportsRepository
 import com.chskela.monoapplication.domain.transaction.models.Transaction
@@ -8,17 +9,18 @@ import com.chskela.monoapplication.mappers.mapToTransaction
 import com.chskela.monoapplication.mappers.mapToTransactionWithCategory
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
-class ReportsRepositoryImpl(private val reportsDao: ReportsDao) :
-    ReportsRepository {
+class ReportsRepositoryImpl(private val reportsDao: ReportsDao) : ReportsRepository,
+    AbstractRepository() {
 
     override fun getAllTransactionsWithCategory(): Flow<List<TransactionWithCategory>> {
         return reportsDao.getAllTransactionsEntityWithCategory()
             .distinctUntilChanged()
             .map { list ->
                 list.map { it.mapToTransactionWithCategory() }
-            }
+            }.flowOn(coroutineContext)
     }
 
     override fun getAllTransactionsByCategory(categoryId: Long): Flow<List<Transaction>> {
@@ -26,6 +28,16 @@ class ReportsRepositoryImpl(private val reportsDao: ReportsDao) :
             .distinctUntilChanged()
             .map { list ->
                 list.map { it.mapToTransaction() }
-            }
+            }.flowOn(coroutineContext)
+    }
+
+    override fun getAmountByCategoryPerMonth(
+        categoryId: Long,
+        startOfMonthInMilliseconds: Long
+    ): Flow<Long> {
+        return reportsDao
+            .getAmountByCategoryPerMonth(categoryId, startOfMonthInMilliseconds)
+            .distinctUntilChanged()
+            .flowOn(coroutineContext)
     }
 }
