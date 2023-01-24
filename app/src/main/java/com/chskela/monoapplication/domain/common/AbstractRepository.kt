fd1a0953081ab8d7a30a33a2622c310601a2f0fc
Chistyakov Alexey <chskela@gmail.com>
2023-01-24 21:53:40 +0300

    Added AbstractRepository

---
+++ app/src/main/java/com/chskela/monoapplication/domain/common/AbstractRepository.kt
@@ -0,0 +1,11 @@
+package com.chskela.monoapplication.domain.common
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+
+abstract class AbstractRepository {
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        throwable.printStackTrace()
+    }
+    protected val coroutineContext by lazy { Dispatchers.IO + coroutineExceptionHandler }
+}
