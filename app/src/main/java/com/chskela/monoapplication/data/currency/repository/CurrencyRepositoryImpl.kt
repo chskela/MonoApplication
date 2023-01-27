699a641f1fe4b9fc1b5e07453c50a66d18f558e1
Chistyakov Alexey <chskela@gmail.com>
2023-01-27 21:46:18 +0300

    Updated CurrencyRepositoryImpl - impl AbstractRepository and add coroutineContext

--- app/src/main/java/com/chskela/monoapplication/data/currency/repository/CurrencyRepositoryImpl.kt
+++ app/src/main/java/com/chskela/monoapplication/data/currency/repository/CurrencyRepositoryImpl.kt
@@ -2,48 +2,60 @@ package com.chskela.monoapplication.data.currency.repository
 
 import com.chskela.monoapplication.data.currency.storage.dao.CurrencyDao
 import com.chskela.monoapplication.data.currency.storage.store.CurrencyStore
+import com.chskela.monoapplication.domain.common.repository.AbstractRepository
 import com.chskela.monoapplication.domain.currency.models.Currency
 import com.chskela.monoapplication.domain.currency.repository.CurrencyRepository
 import com.chskela.monoapplication.mappers.mapToCurrency
 import com.chskela.monoapplication.mappers.mapToCurrencyEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 class CurrencyRepositoryImpl(
     private val currencyDao: CurrencyDao,
     private val currencyStore: CurrencyStore
-) : CurrencyRepository {
+) : CurrencyRepository, AbstractRepository() {
 
     override fun getListCurrency(): Flow<List<Currency>> {
         return currencyDao.getListCurrency()
             .distinctUntilChanged()
-            .map { list ->
-                list.map { it.mapToCurrency() }
-            }
+            .map { list -> list.map { it.mapToCurrency() } }
+            .flowOn(coroutineContext)
     }
 
-    override suspend fun getCurrencyById(id: Long): Currency {
-        return currencyDao.getCurrencyById(id).mapToCurrency()
+    override suspend fun getCurrencyById(id: Long): Currency = withContext(coroutineContext) {
+        currencyDao.getCurrencyById(id).mapToCurrency()
     }
 
     override fun getIdDefaultCurrency(): Flow<Long> {
-        return currencyStore.getDefaultCurrency.distinctUntilChanged()
+        return currencyStore.getDefaultCurrency
+            .distinctUntilChanged()
+            .flowOn(coroutineContext)
     }
 
     override suspend fun insertCurrency(currency: Currency) {
-        currencyDao.insertCurrency(currency.mapToCurrencyEntity())
+        withContext(coroutineContext) {
+            currencyDao.insertCurrency(currency.mapToCurrencyEntity())
+        }
     }
 
     override suspend fun updateCurrency(currency: Currency) {
-        currencyDao.updateCurrency(currency.mapToCurrencyEntity())
+        withContext(coroutineContext) {
+            currencyDao.updateCurrency(currency.mapToCurrencyEntity())
+        }
     }
 
     override suspend fun deleteCurrency(currency: Currency) {
-        currencyDao.deleteCurrency(currency.mapToCurrencyEntity())
+        withContext(coroutineContext) {
+            currencyDao.deleteCurrency(currency.mapToCurrencyEntity())
+        }
     }
 
     override suspend fun setDefaultCurrency(id: Long) {
-        currencyStore.saveDefaultCurrency(id)
+        withContext(coroutineContext) {
+            currencyStore.saveDefaultCurrency(id)
+        }
     }
 }
