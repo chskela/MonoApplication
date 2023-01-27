e42d6a19745ac8f28af1c2ef7cb40f9011306596
Chistyakov Alexey <chskela@gmail.com>
2023-01-27 21:47:08 +0300

    Updated OnBoardingRepositoryImpl - impl AbstractRepository and add coroutineContext

--- app/src/main/java/com/chskela/monoapplication/data/onboarding/repository/OnBoardingRepositoryImpl.kt
+++ app/src/main/java/com/chskela/monoapplication/data/onboarding/repository/OnBoardingRepositoryImpl.kt
@@ -1,15 +1,21 @@
 package com.chskela.monoapplication.data.onboarding.repository
 
 import com.chskela.monoapplication.data.onboarding.storage.store.OnBoardingStore
+import com.chskela.monoapplication.domain.common.repository.AbstractRepository
 import com.chskela.monoapplication.domain.onboarding.repository.OnBoardingRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 
-class OnBoardingRepositoryImpl(private val onBoardingStore: OnBoardingStore) : OnBoardingRepository {
+class OnBoardingRepositoryImpl(private val onBoardingStore: OnBoardingStore) : OnBoardingRepository,
+    AbstractRepository() {
     override fun onBoardingIsSkip(): Flow<Boolean> {
-        return onBoardingStore.onBoardingIsSkip
+        return onBoardingStore.onBoardingIsSkip.flowOn(coroutineContext)
     }
 
     override suspend fun setOnBoardingIsSkip() {
-        onBoardingStore.setOnBoardingIsSkip()
+        withContext(coroutineContext) {
+            onBoardingStore.setOnBoardingIsSkip()
+        }
     }
 }
