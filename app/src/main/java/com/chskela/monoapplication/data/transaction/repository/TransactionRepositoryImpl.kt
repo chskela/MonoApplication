8a482c18253c827059845d173d3603ec9248ad7f
Chistyakov Alexey <chskela@gmail.com>
2023-01-27 21:48:39 +0300

    Updated TransactionRepositoryImpl - impl AbstractRepository and add coroutineContext

--- app/src/main/java/com/chskela/monoapplication/data/transaction/repository/TransactionRepositoryImpl.kt
+++ app/src/main/java/com/chskela/monoapplication/data/transaction/repository/TransactionRepositoryImpl.kt
@@ -2,30 +2,41 @@ package com.chskela.monoapplication.data.transaction.repository
 
 import com.chskela.monoapplication.data.transaction.storage.dao.TransactionDao
 import com.chskela.monoapplication.data.reports.storage.models.TransactionEntityWithCategory
+import com.chskela.monoapplication.domain.common.repository.AbstractRepository
 import com.chskela.monoapplication.domain.transaction.models.Transaction
 import com.chskela.monoapplication.domain.transaction.repository.TransactionRepository
 import com.chskela.monoapplication.mappers.mapToTransactionEntity
+import kotlinx.coroutines.withContext
 
 class TransactionRepositoryImpl(private val transactionDao: TransactionDao) :
-    TransactionRepository {
+    TransactionRepository, AbstractRepository() {
 
-    override suspend fun getTransactionById(id: Long): TransactionEntityWithCategory {
-        return transactionDao.getTransactionById(id)
-    }
+    override suspend fun getTransactionById(id: Long): TransactionEntityWithCategory =
+        withContext(coroutineContext) {
+            transactionDao.getTransactionById(id)
+        }
 
     override suspend fun insertTransaction(transaction: Transaction) {
-        transactionDao.insertTransaction(transaction.mapToTransactionEntity())
+        withContext(coroutineContext) {
+            transactionDao.insertTransaction(transaction.mapToTransactionEntity())
+        }
     }
 
     override suspend fun updateTransaction(transaction: Transaction) {
-        transactionDao.updateTransaction(transaction.mapToTransactionEntity())
+        withContext(coroutineContext) {
+            transactionDao.updateTransaction(transaction.mapToTransactionEntity())
+        }
     }
 
     override suspend fun deleteTransaction(transaction: Transaction) {
-        transactionDao.deleteTransaction(transaction.mapToTransactionEntity())
+        withContext(coroutineContext) {
+            transactionDao.deleteTransaction(transaction.mapToTransactionEntity())
+        }
     }
 
     override suspend fun deleteAllTransactions() {
-        transactionDao.deleteAllTransactions()
+        withContext(coroutineContext) {
+            transactionDao.deleteAllTransactions()
+        }
     }
 }
