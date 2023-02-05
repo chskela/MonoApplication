1b584dd16d50f505bc10a138961d2dcade0a52e3
Chistyakov Alexey <chskela@gmail.com>
2023-02-05 23:15:26 +0300

    Added TestTags - constants for test

---
+++ app/src/main/java/com/chskela/monoapplication/core/util/TestTags.kt
@@ -0,0 +1,8 @@
+package com.chskela.monoapplication.core.util
+
+object TestTags {
+
+    const val ON_BOARDING_PAGE_NUMBER = "ON_BOARDING_PAGE_NUMBER"
+    const val ON_BOARDING_SKIP_BUTTON = "ON_BOARDING_SKIP_BUTTON"
+    const val ON_BOARDING_CONTINUE_BUTTON = "ON_BOARDING_CONTINUE_BUTTON"
+}
