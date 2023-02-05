4bf83c25bfe733dde43d6c92e89069f4f3534627
Chistyakov Alexey <chskela@gmail.com>
2023-02-05 19:36:06 +0300

    Updated CurrencyFormatUseCase - add param local and throw IllegalArgumentException

--- app/src/main/java/com/chskela/monoapplication/domain/common/usecase/CurrencyFormatUseCase.kt
+++ app/src/main/java/com/chskela/monoapplication/domain/common/usecase/CurrencyFormatUseCase.kt
@@ -3,11 +3,17 @@ package com.chskela.monoapplication.domain.common.usecase
 import java.text.NumberFormat
 import java.util.*
 
-class CurrencyFormatUseCase {
-    private val numberFormat: NumberFormat = NumberFormat.getCurrencyInstance()
+class CurrencyFormatUseCase(locale: Locale = Locale("ru")) {
+    private val numberFormat: NumberFormat = NumberFormat.getCurrencyInstance(locale)
 
+    @Throws(IllegalArgumentException::class)
     operator fun invoke(letterCode: String): (Double) -> String {
-        numberFormat.currency = Currency.getInstance(letterCode)
+        try {
+            numberFormat.currency = Currency.getInstance(letterCode)
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException("letterCode is not a supported ISO 4217 code")
+        }
+
 
         return { number -> numberFormat.format(number) }
     }
