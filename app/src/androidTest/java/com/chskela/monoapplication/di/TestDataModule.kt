e13285584e3ef15138d682970753a38937f4e4ff
Chistyakov Alexey <chskela@gmail.com>
2023-02-05 22:49:21 +0300

    Added DI for tests

---
+++ app/src/androidTest/java/com/chskela/monoapplication/di/TestDataModule.kt
@@ -0,0 +1,90 @@
+package com.chskela.monoapplication.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.chskela.monoapplication.data.category.repository.CategoryRepositoryImpl
+import com.chskela.monoapplication.data.currency.repository.CurrencyRepositoryImpl
+import com.chskela.monoapplication.data.currency.storage.store.CurrencyStore
+import com.chskela.monoapplication.data.database.AppDatabase
+import com.chskela.monoapplication.data.reports.repository.ReportsRepositoryImpl
+import com.chskela.monoapplication.data.onboarding.repository.OnBoardingRepositoryImpl
+import com.chskela.monoapplication.data.onboarding.storage.store.OnBoardingStore
+import com.chskela.monoapplication.data.transaction.repository.TransactionRepositoryImpl
+import com.chskela.monoapplication.domain.category.repository.CategoryRepository
+import com.chskela.monoapplication.domain.currency.repository.CurrencyRepository
+import com.chskela.monoapplication.domain.reports.repository.ReportsRepository
+import com.chskela.monoapplication.domain.onboarding.repository.OnBoardingRepository
+import com.chskela.monoapplication.domain.onboarding.usecase.OnBoardingIsSkipUseCase
+import com.chskela.monoapplication.domain.transaction.repository.TransactionRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestDataModule {
+
+    @Provides
+    @Singleton
+    fun provideCurrencyDatabase(app: Application): AppDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            app,
+            AppDatabase::class.java,
+        )
+            .createFromAsset("database/mono.db")
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCurrencyStore(@ApplicationContext appContext: Context): CurrencyStore {
+        return CurrencyStore(appContext)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCurrencyRepository(db: AppDatabase, currencyStore: CurrencyStore): CurrencyRepository {
+        return CurrencyRepositoryImpl(db.currencyDao, currencyStore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCategoryRepository(db: AppDatabase): CategoryRepository {
+        return CategoryRepositoryImpl(db.categoryDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTransactionRepository(db: AppDatabase): TransactionRepository {
+        return TransactionRepositoryImpl(db.transactionDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMonthReportRepository(db: AppDatabase): ReportsRepository {
+        return ReportsRepositoryImpl(db.reportsDao)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideOnBoardingStore(@ApplicationContext appContext: Context): OnBoardingStore {
+        return OnBoardingStore(appContext)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSettingsRepository(onBoardingStore: OnBoardingStore): OnBoardingRepository {
+        return OnBoardingRepositoryImpl(onBoardingStore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOnBoardingIsSkipUseCase(repository: OnBoardingRepository) : OnBoardingIsSkipUseCase {
+        return OnBoardingIsSkipUseCase(repository)
+    }
+}
---
+++ app/src/androidTest/java/com/chskela/monoapplication/di/TestDomainModule.kt
@@ -0,0 +1,86 @@
+package com.chskela.monoapplication.di
+
+import com.chskela.monoapplication.domain.category.repository.CategoryRepository
+import com.chskela.monoapplication.domain.category.usecase.*
+import com.chskela.monoapplication.domain.common.usecase.CurrencyFormatUseCase
+import com.chskela.monoapplication.domain.currency.repository.CurrencyRepository
+import com.chskela.monoapplication.domain.currency.usecase.*
+import com.chskela.monoapplication.domain.reports.repository.ReportsRepository
+import com.chskela.monoapplication.domain.reports.usecase.*
+import com.chskela.monoapplication.domain.onboarding.repository.OnBoardingRepository
+import com.chskela.monoapplication.domain.onboarding.usecase.SetOnBoardingIsSkipUseCase
+import com.chskela.monoapplication.domain.transaction.repository.TransactionRepository
+import com.chskela.monoapplication.domain.transaction.usercase.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object TestDomainModule {
+
+    @Provides
+    fun provideCurrencyUseCases(repository: CurrencyRepository): CurrencyUseCases {
+        return CurrencyUseCases(
+            addCurrencyUseCase = AddCurrencyUseCase(repository),
+            updateCurrencyUseCase = UpdateCurrencyUseCase(repository),
+            deleteCurrencyUseCase = DeleteCurrencyUseCase(repository),
+            getDefaultCurrencyUseCase = GetDefaultCurrencyUseCase(repository),
+            getListCurrencyUseCase = GetListCurrencyUseCase(repository),
+            setDefaultCurrencyUseCase = SetDefaultCurrencyUseCase(repository),
+            getCurrencyByIdUseCase = GetCurrencyByIdUseCase(repository)
+        )
+    }
+
+    @Provides
+    fun provideCategoryUseCases(repository: CategoryRepository): CategoryUseCases {
+        return CategoryUseCases(
+            getAllCategoryUseCase = GetAllCategoryUseCase(repository),
+            addCategoryUseCase = AddCategoryUseCase(repository),
+            deleteCategoryUseCase = DeleteCategoryUseCase(repository),
+            updateCategoryUseCase = UpdateCategoryUseCase(repository),
+            getCategoryByIdUseCase = GetCategoryByIdUseCase(repository)
+        )
+    }
+
+    @Provides
+    fun provideTransactionUseCases(repository: TransactionRepository): TransactionUseCases {
+        return TransactionUseCases(
+            addTransactionUseCase = AddTransactionUseCase(repository),
+            deleteTransactionUseCase = DeleteTransactionUseCase(repository),
+            getTransactionByIdUseCase = GetTransactionByIdUseCase(repository),
+            updateTransactionUseCase = UpdateTransactionUseCase(repository)
+        )
+    }
+
+    @Provides
+    fun provideDeleteAllTransactionsUseCase(repository: TransactionRepository): DeleteAllTransactionsUseCase {
+        return DeleteAllTransactionsUseCase(repository)
+    }
+
+    @Provides
+    fun provideGetAllTransactionsUseCase(repository: ReportsRepository): GetAllTransactionsUseCase {
+        return GetAllTransactionsUseCase(repository)
+    }
+
+    @Provides
+    fun provideGetAllTransactionsByCategoryUseCase(repository: ReportsRepository): GetAllTransactionsByMonthAndCategoryUseCase {
+        return GetAllTransactionsByMonthAndCategoryUseCase(repository)
+    }
+
+    @Provides
+    fun provideGetAmountByCategoryPerMonthUseCase(repository: ReportsRepository): GetAmountByCategoryPerMonthUseCase {
+        return GetAmountByCategoryPerMonthUseCase(repository)
+    }
+
+    @Provides
+    fun provideSetOnBoardingIsSkipUseCase(repository: OnBoardingRepository): SetOnBoardingIsSkipUseCase {
+        return SetOnBoardingIsSkipUseCase(repository)
+    }
+
+    @Provides
+    fun provideCurrencyFormatUseCase(): CurrencyFormatUseCase {
+        return CurrencyFormatUseCase()
+    }
+}
