145a4a11ecb1e9dc955cd0c1bd183bd35e2eddf0
Chistyakov Alexey <chskela@gmail.com>
2023-02-08 22:05:13 +0300

    Updated CategoryRepository - add getAllCategoryByType

--- app/src/main/java/com/chskela/monoapplication/domain/category/repository/CategoryRepository.kt
+++ app/src/main/java/com/chskela/monoapplication/domain/category/repository/CategoryRepository.kt
@@ -1,11 +1,14 @@
 package com.chskela.monoapplication.domain.category.repository
 
 import com.chskela.monoapplication.domain.category.models.Category
+import com.chskela.monoapplication.domain.category.models.TypeCategory
 import kotlinx.coroutines.flow.Flow
 
 interface CategoryRepository {
 
     fun getAllCategory() : Flow<List<Category>>
+
+    fun getAllCategoryByType(type: TypeCategory) : Flow<List<Category>>
 
     fun getCategoryById(id: Long) : Flow<Category>
 
