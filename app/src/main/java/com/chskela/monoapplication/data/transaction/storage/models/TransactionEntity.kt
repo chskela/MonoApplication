7fc75852a48da6b13c4c0bfe77cd866d3b6a7fdd
Chistyakov Alexey <chskela@gmail.com>
2023-02-14 22:22:18 +0300

    Updated TransactionEntity - change type on Date

--- app/src/main/java/com/chskela/monoapplication/data/transaction/storage/models/TransactionEntity.kt
+++ app/src/main/java/com/chskela/monoapplication/data/transaction/storage/models/TransactionEntity.kt
@@ -3,12 +3,13 @@ package com.chskela.monoapplication.data.transaction.storage.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "transaction")
 data class TransactionEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
-    val timestamp: Long,
+    val timestamp: Date,
     val amount: Long,
     val note: String,
     @ColumnInfo(name = "category_id")
