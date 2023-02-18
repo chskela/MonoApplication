d604e2118fa9e5f276bca7201c136172902aeed0
Chistyakov Alexey <chskela@gmail.com>
2023-02-18 18:44:59 +0300

    Updated GetAmountByCategoryPerMonthUseCase - remove getBeginningOfMonth

--- app/src/main/java/com/chskela/monoapplication/domain/reports/usecase/GetAmountByCategoryPerMonthUseCase.kt
+++ app/src/main/java/com/chskela/monoapplication/domain/reports/usecase/GetAmountByCategoryPerMonthUseCase.kt
@@ -1,28 +1,20 @@
 package com.chskela.monoapplication.domain.reports.usecase
 
+import com.chskela.monoapplication.domain.common.usecase.BeginningOfMonthUseCase
 import com.chskela.monoapplication.domain.reports.repository.ReportsRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import java.util.*
-import java.util.Calendar.*
 
-class GetAmountByCategoryPerMonthUseCase(private val reportsRepository: ReportsRepository) {
+class GetAmountByCategoryPerMonthUseCase(
+    private val reportsRepository: ReportsRepository,
+    private val beginningOfMonthUseCase: BeginningOfMonthUseCase
+    ) {
 
     operator fun invoke(categoryId: Long): Flow<Double> {
 
         return reportsRepository.getAmountByCategoryPerMonth(
             categoryId = categoryId,
-            startOfMonth = getBeginningOfMonth()
+            startOfMonth = beginningOfMonthUseCase()
         ).map { it / 100.0 }
-    }
-
-    private fun getBeginningOfMonth(): Date {
-        val startOfMonth = getInstance()
-        startOfMonth.set(DAY_OF_MONTH, 1)
-        startOfMonth.set(HOUR_OF_DAY, 0)
-        startOfMonth.set(MINUTE, 0)
-        startOfMonth.set(SECOND, 0)
-        startOfMonth.set(MILLISECOND, 0)
-        return startOfMonth.time
     }
 }
