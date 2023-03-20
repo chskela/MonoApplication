7adf075672f4751754856eaa8bef58838cd4aa44
Chistyakov Alexey <chskela@gmail.com>
2023-03-20 20:24:56 +0300

    Added IsDarkThemeUseCase

---
+++ app/src/main/java/com/chskela/monoapplication/domain/themeswitcher/usecase/IsDarkThemeUseCase.kt
@@ -0,0 +1,12 @@
+package com.chskela.monoapplication.domain.themeswitcher.usecase
+
+import com.chskela.monoapplication.domain.themeswitcher.repository.ThemeSwitcher
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class IsDarkThemeUseCase @Inject constructor(private val themeSwitcher: ThemeSwitcher) {
+
+    operator fun invoke(): Flow<Boolean?> {
+        return themeSwitcher.isDarkTheme()
+    }
+}
