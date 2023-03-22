d7c188df20fa012690411d14a58f5491f8686859
Chistyakov Alexey <chskela@gmail.com>
2023-03-22 12:39:37 +0300

    Updated SettingsViewModel - add isDarkTheme and setTheme

--- app/src/main/java/com/chskela/monoapplication/presentation/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/screens/settings/SettingsViewModel.kt
@@ -2,19 +2,38 @@ package com.chskela.monoapplication.presentation.screens.settings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.chskela.monoapplication.domain.themeswitcher.usecase.IsDarkThemeUseCase
+import com.chskela.monoapplication.domain.themeswitcher.usecase.SetDarkThemeUseCase
 import com.chskela.monoapplication.domain.transaction.usercase.DeleteAllTransactionsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    private val deleteAllTransactionsUseCase: DeleteAllTransactionsUseCase,
+    isDarkThemeUseCase: IsDarkThemeUseCase,
+    private val setDarkThemeUseCase: SetDarkThemeUseCase,
+    private val deleteAllTransactionsUseCase: DeleteAllTransactionsUseCase
 ) : ViewModel() {
+
+    val isDarkTheme: StateFlow<Boolean> = isDarkThemeUseCase().stateIn(
+        viewModelScope,
+        started = SharingStarted.Eagerly,
+      true
+    )
 
     fun deleteAllData() {
         viewModelScope.launch {
             deleteAllTransactionsUseCase()
+        }
+    }
+
+    fun setTheme(value: Boolean){
+        viewModelScope.launch {
+            setDarkThemeUseCase(value)
         }
     }
 }
