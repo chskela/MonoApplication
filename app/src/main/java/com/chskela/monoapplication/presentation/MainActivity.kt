5724a3b2e420f8e8ffe01db98f499421a9274223
Chistyakov Alexey <chskela@gmail.com>
2023-03-22 12:38:03 +0300

    Updated MainActivity - set default theme

--- app/src/main/java/com/chskela/monoapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/chskela/monoapplication/presentation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.chskela.monoapplication.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,12 +18,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        installSplashScreen().setKeepOnScreenCondition{
+        installSplashScreen().setKeepOnScreenCondition {
             !mainActivityViewModel.isLoading.value
         }
 
         setContent {
-            MonoApp(onBoardingIsSkip = mainActivityViewModel.onBoardingIsSkip.value)
+            val isDark = isSystemInDarkTheme()
+            LaunchedEffect(true) {
+                mainActivityViewModel.setTheme(isDark)
+            }
+
+            MonoApp(
+                onBoardingIsSkip = mainActivityViewModel.onBoardingIsSkip.value,
+                darkTheme = isAppDarkTheme(mainActivityViewModel.isDarkTheme)
+            )
         }
     }
 }
